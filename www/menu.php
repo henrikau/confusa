566a735b61a30c4fbb1d034ef2347ef9b76e873d
Henrik Austad <henrikau@stud.ntnu.no>
2008-07-14 15:10:43 +0200

    renamed key_gen to key_handler (which is more approriate given the tasks key_handler actually performs

--- www/menu.php
+++ www/menu.php
@@ -7,20 +7,11 @@ function render_menu($authenticated)
     echo "<A HREF=\"index.php\">Front</A><BR><BR>\n";
 
     if ($authenticated) {
-        echo "<A HREF=\"key_gen.php\">Key</A><BR>\n";
+        echo "<A HREF=\"key_handler.php\">Key</A><BR>\n";
         echo feide_logout_link("logout.php", "Logout") . "<BR>\n";
     }
     else {
         include_once('login_link.html');
     }
-    echo "<BR><BR><BR>\n";
-    /* we *have* to inform users of what Confusa is, before
-     * we demand that they log in. Also, we must provide the
-     * root-certs etc to all users
-     */
-/*     echo ("<A HREF=\"about.php\">About SLCS-web</A><BR>\n"); */
-/*     echo ("<BR>\n"); */
-/*     echo ("<A HREF=\"documentation.php\">SysDoc</A><BR>\n"); */
-/*     echo ("<BR>\n"); */
-        }
+    } /* end render_menu */
 ?>
