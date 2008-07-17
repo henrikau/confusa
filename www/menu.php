69696d3c88cdc2e67e8ffe4a0cce20d2639e5f30
Henrik Austad <henrik.austad@gmail.com>
2008-07-17 15:34:58 +0200

    added link to root-certificate in menu
    Signed-off-by: Henrik Austad <henrik.austad@gmail.com>

--- www/menu.php
+++ www/menu.php
@@ -4,11 +4,12 @@ function render_menu($authenticated)
     echo "<B>Menu</B><BR/><BR>\n";
     /* always show the Frontpage-link (this page should always be
      * visible */
-    echo "<A HREF=\"index.php\">Front</A><BR><BR>\n";
+    echo "<A HREF=\"index.php\">Start</A><BR><BR>\n";
+    echo "<A HREF=\"root_cert.php\">CA-Cert.</A><BR><BR>\n";
 
     if ($authenticated) {
-        echo "<A HREF=\"key_handler.php\">Key</A><BR>\n";
-        echo feide_logout_link("logout.php", "Logout") . "<BR>\n";
+        echo "<A HREF=\"key_handler.php\">Keys</A><BR><BR>\n";
+        echo feide_logout_link("logout.php", "Logout") . "<BR><BR>\n";
     }
     else {
         include_once('login_link.html');
