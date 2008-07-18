6be1ac7067be41196cc18c85e5bab26dd539fc4e
Henrik Austad <henrik.austad@gmail.com>
2008-07-18 14:44:02 +0200

    handle download of files through the browser by proper header-rewriting
    Signed-off-by: Henrik Austad <henrik.austad@gmail.com>

---
+++ lib/file_download.php
@@ -0,0 +1,20 @@
+<?php
+/* download_file()
+ *
+ * Creates a download to the user 
+ */
+function download_file($file, $filename)
+{
+     header('Content-Type: application/force-download');
+     header('Content-Disposition: attachment; filename="'.$filename.'"');
+     header('Content-Transfer-Encoding: binary');
+     header('Accept-Ranges: bytes');
+     header('Content-Length: ' . strlen($file));
+
+     /* turn of caching */
+     header("Cache-control: private");
+     header('Pragma: private');
+     header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+     echo $file;
+}
+?>
