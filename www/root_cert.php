8f76cde15a171398484c86d411114877d9f61100
Henrik Austad <henrik.austad@gmail.com>
2008-09-11 09:29:28 +0200

    added direct download for root-certificate

--- www/root_cert.php
+++ www/root_cert.php
@@ -1,12 +1,30 @@
 <?php
 include_once('framework.php');
+
+if (isset($_GET['send_file'])) {
+     include_once('file_download.php');
+     download_file(file_get_contents("certs/sigma_cert.pem"), "sigma_cert.pem");
+     exit(1);
+}
+
 $fw = new Framework('root_cert');
 $fw->render_page();
 
 function root_cert($person)
 {
-  echo "This is the Certificate we use for signing the CSRs we receive.<br>\n";
-  echo "If you want the certificate, it's <A HREF=\"certs/sigma_cert.pem\">here</A><BR>\n";
+     echo "<P>\n";
+     echo "This is the Certificate we use for signing the CSRs we receive.<br>\n";
+     echo "If you want the certificate, it's <A HREF=\"certs/sigma_cert.pem\">here</A><BR>\n";
+     echo "</P>\n";
+
+     echo "<P>\n";
+     echo "Or, if you want to download it directly, press here:  ";
+     echo '<form method="GET" action="root_cert.php">';
+     echo '<input type="hidden" name="send_file">';
+     echo '<input type="submit" value="root cert">';
+     echo '</form>';
+     echo "</P>\n";
+
 
   /* read and display the key in a nicely formatted way */
   $content = file_get_contents("certs/sigma_cert.pem");
