6ad590d13959ef81e3b45a547d2c1b2baa4a3283
Henrik Austad <henrik.austad@gmail.com>
2008-09-15 11:07:49 +0200

    added news entry in database/index.php

--- www/index.php
+++ www/index.php
@@ -7,12 +7,23 @@ $fw->render_page();
 
 function welcome($person) 
 {
-    if ($person->is_auth())
-        {
-             echo "This is the information we have received about you: <BR>\n";
-             echo $person . "<BR>\n";
-        }
-    include('unclassified_intro.php');
+     require_once('mdb2_wrapper.php');
+     echo "<h3>News</h3>\n";
+     $val = 0;
+     $res = MDB2Wrapper::execute("SELECT news, issued FROM news WHERE newsid>?", array('integer'), array($val));
+
+     foreach ($res as $key => $line) {
+          echo "<P>\n";
+          echo "<ADDRESS>".htmlentities($line['issued'])."</ADDRESS>\n";
+          echo htmlentities($line['news'])."\n";
+          echo "</P>\n";
+     }
+     if ($person->is_auth())
+     {
+          echo "This is the information we have received about you: <BR>\n";
+          echo $person . "<BR>\n";
+     }
+     include('unclassified_intro.php');
 }
 ?>
 
