39727f23f1a0d800d9fa60fd0c735c0bd5aadbea
Henrik Austad <henrik.austad@gmail.com>
2008-10-08 14:02:57 +0200

    initial commit of applet part (most work done for sane build and jar-creationf

---
+++ applet/src/confusa/ConfusaAppLocal.java
@@ -0,0 +1,14 @@
+package confusa;
+
+import javax.swing.*;
+
+public class ConfusaAppLocal {
+     public static void main(String[] args) {
+          System.out.println("Hello world!");
+          JFrame jr = new JFrame("confusa app v.2");
+          
+          jr.setSize(800,600);
+          jr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          jr.setVisible(true);
+     } // end main
+} // end ConfusaAppLocal()
