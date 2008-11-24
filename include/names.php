d838d9eb3ccecf35e3351c670ac68fa21ba669cc
Henrik Austad <henrik@austad.us>
2008-11-24 16:47:23 +0100

    add finland to known idps, and map attributes so we can understand them
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- include/names.php
+++ include/names.php
@@ -15,7 +15,9 @@ function attributealter_names(&$attributes, $spentityid = null, $idpentityid = n
       */
      $known_idps = array('max.feide.no' => 'NO',
                          'edugain.showcase.surfnet.nl' => 'DL',
-                         'https://testidp.wayf.dk' => 'DK');
+                         'https://testidp.wayf.dk' => 'DK',
+                         'https://aitta2.funet.fi/idp/shibboleth' => 'FI'
+                         );
 
      $attributes['country'] = array($known_idps[$idpentityid]);
      if (!$attributes['country']) {
@@ -28,10 +30,18 @@ function attributealter_names(&$attributes, $spentityid = null, $idpentityid = n
      }
 
      /* fix shortnames -> add to feide-compatible */
+
+     /* SurfNET */
      if (isset($attributes['urn:mace:dir:attribute-def:eduPersonPrincipalName'][0])) {
           $attributes['eduPersonPrincipalName'] = array($attributes['urn:mace:dir:attribute-def:eduPersonPrincipalName'][0]);
           $attributes['cn']                     = array($attributes['urn:mace:dir:attribute-def:cn'][0]);
           $attributes['mail']                   = array($attributes['urn:mace:dir:attribute-def:mail'][0]);
+     }
+     /* HAKA */
+     else if (isset($attributes['urn:oid:1.3.6.1.4.1.5923.1.1.1.6'])) {
+          $attributes['eduPersonPrincipalName'] = array($attributes['urn:oid:1.3.6.1.4.1.5923.1.1.1.6'][0]);
+          $attributes['cn']                     = array($attributes['urn:oid:2.5.4.3'][0]);
+          $attributes['mail']                   = array($attributes['urn:oid:0.9.2342.19200300.100.1.3'][0]);
      }
 }
 ?>
