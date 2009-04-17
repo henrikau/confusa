29b6438d1fb1dbf5c9300749baaf0d240d0d0ae8
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-04-17 15:57:36 +0200

    Make it possible to show only the DN to the client
    This is a rework of Thomas' original patch.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- www/about_you.php
+++ www/about_you.php
@@ -6,11 +6,24 @@ $fw->force_login();
 $fw->render_page();
 
 function about_you($person) {
-	echo "<H3>This is what we know about you:</H3>\n";
-	echo $person;
-	echo "<HR>\n";
-	echo "we store very little information. What we do keep, is information about certificates issued, combinded with the eduPersonPrincipalName\n";
-	echo "This is the DN in the certificate, and whe <b>have</b> to store this.<BR>\n";
+	$textual = "no";
+
+	if (isset($_GET['text'])) {
+		$textual = htmlentities($_GET['text']);
+	}
+
+	if ($textual == "yes") {
+		/* The id makes it very easy to automatically retrieve the content, e.g. with XPath */
+		echo "<DIV ID=\"dn-section\">";
+		echo $person->get_complete_dn();
+		echo "</DIV>";
+	} else {
+		echo "<H3>This is what we know about you:</H3>\n";
+		echo $person;
+		echo "<HR>\n";
+		echo "we store very little information. What we do keep, is information about certificates issued, combinded with the eduPersonPrincipalName\n";
+		echo "This is the DN in the certificate, and whe <b>have</b> to store this.<BR>\n";
+	}
 	
 }
 ?>
