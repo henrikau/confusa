51df6313086abc7900a474ab09d01be37c388863
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-06-05 11:55:58 +0200

    Removed verification of CSRs when using remote API
    This is not needed any more because the Comodo API will overwrite all
    subject fields and extensions in the issued certificate.
    
    We specifiy what will be in the cert by the API call. Unfortunately, this
    changed the whole sign_key() method. Took the opportunity to remove
    multiple return points.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- lib/key/cert_manager.php
+++ lib/key/cert_manager.php
@@ -67,48 +67,56 @@ class CertManager
    */
   function sign_key($auth_key)
     {
-     if ($this->verify_csr()) {
-		  $cert_path = 'file://'.dirname(WEB_DIR) . Config::get_config('ca_cert_path') . Config::get_config('ca_cert_name');
-		  $ca_priv_path = 'file://'.dirname(WEB_DIR) . Config::get_config('ca_key_path') . Config::get_config('ca_key_name');
+      $success = false;
+     /* Standalone mode, use php and local certificate/key to
+      * sign for user */
+     if (Config::get_config('standalone')) {
+       if ($this->verify_csr()) {
+		      $cert_path = 'file://'.dirname(WEB_DIR) . Config::get_config('ca_cert_path') . Config::get_config('ca_cert_name');
+		      $ca_priv_path = 'file://'.dirname(WEB_DIR) . Config::get_config('ca_key_path') . Config::get_config('ca_key_name');
 
-              /* Standalone mode, use php and local certificate/key to
-             * sign for user */
-              if (Config::get_config('standalone')) {
-                 $sign_days = 11;
-                 $tmp_cert = openssl_csr_sign($this->user_csr, $cert_path, $ca_priv_path, $sign_days , array('digest_alg' => 'sha1'));
-                 openssl_x509_export($tmp_cert, $this->user_cert, true);
-                 /* echo __FILE__ .":".__LINE__ ." Certificate successfully signed. <BR>\n"; */
-                 
+          $sign_days = 11;
+          $tmp_cert = openssl_csr_sign($this->user_csr, $cert_path, $ca_priv_path, $sign_days , array('digest_alg' => 'sha1'));
+          openssl_x509_export($tmp_cert, $this->user_cert, true);
+          /* echo __FILE__ .":".__LINE__ ." Certificate successfully signed. <BR>\n"; */
 
-                    MDB2Wrapper::update("INSERT INTO cert_cache (cert, auth_key, cert_owner, valid_untill) VALUES(?, ?, ?, addtime(current_timestamp(), ?))",
-                                        array('text', 'text', 'text', 'text'),
-                                        array($this->user_cert, $auth_key, $this->person->get_valid_cn(), Config::get_config('cert_default_timeout')));
-		    Logger::log_event(LOG_INFO, "Certificate successfully signed for ".
-                                      $this->person->get_valid_cn().
-                                      " Contacting us from ".
-                                      $_SERVER['REMOTE_ADDR']);
 
-		    /* add to database (the hash of the pubkey) */
-		    MDB2Wrapper::update("INSERT INTO pubkeys (pubkey_hash, uploaded_nr) VALUES(?, 0)",
-                                        array('text'),
-                                        array($this->pubkey_checksum));
-		    return true;
-              }
-              else {
-                   /* external CA, send the CSR as signed */
-                   Logger::log_event(LOG_DEBUG, "Signing key using remote CA");
-                   if ($this->capi_upload_csr($auth_key)) {
-                     return $this->capi_authorize_csr();
-                   } else {
-                     return false;
-                   }
-                   
-              }
-         }
-         Logger::log_event(LOG_INFO, "Will not sign invalid CSR for user ".
-                           $this->person->get_valid_cn().
-                           " from ip ".$_SERVER['REMOTE_ADDR']);
-         return false;
+          MDB2Wrapper::update("INSERT INTO cert_cache (cert, auth_key, cert_owner, valid_untill) VALUES(?, ?, ?, addtime(current_timestamp(), ?))",
+                              array('text', 'text', 'text', 'text'),
+                              array($this->user_cert, $auth_key, $this->person->get_valid_cn(), Config::get_config('cert_default_timeout')));
+          Logger::log_event(LOG_INFO, "Certificate successfully signed for ".
+                                $this->person->get_valid_cn().
+                                " Contacting us from ".
+                                $_SERVER['REMOTE_ADDR']);
+
+          $success = true;
+        } else {
+          Logger::log_event(LOG_INFO, "Will not sign invalid CSR for user ".
+                       $this->person->get_valid_cn().
+                       " from ip ".$_SERVER['REMOTE_ADDR']);
+          $success = false;
+        }
+     }
+      else {
+           /* external CA, send the CSR as signed */
+           Logger::log_event(LOG_DEBUG, "Signing key using remote CA");
+           echo "uploading csr...<br />\n";
+           if ($this->capi_upload_csr($auth_key)) {
+             echo "authorizing csr...<br />\n";
+             $success = $this->capi_authorize_csr();
+           } else {
+             $success = false;
+           }
+      }
+
+      /* add to database (the hash of the pubkey) if signing the certificate was successful */
+      if ($success) {
+        MDB2Wrapper::update("INSERT INTO pubkeys (pubkey_hash, uploaded_nr) VALUES(?, 0)",
+                                    array('text'),
+                                    array($this->pubkey_checksum));
+      }
+
+      return $success;
     }  /*end sign_key() */ 
 
   /*
