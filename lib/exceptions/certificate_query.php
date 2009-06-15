5c391a468cb203d8702dd32449d673bcca3bbfe4
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-06-15 10:14:42 +0200

    Added CertificateQueryException
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

---
+++ lib/exceptions/certificate_query.php
@@ -0,0 +1,26 @@
+<?php
+declare(encoding = 'utf-8');
+require_once('confusa_gen.php');
+
+/**
+ * CertificateQueryException thrown if a query for a certificate list does not
+ * return the expected result.
+ * Exception stub that can be extended in the future.
+ *
+ * PHP version 5
+ * @author Thomas Zangerl <tzangerl@pdc.kth.se>
+ */
+class CertificateQueryException extends ConfusaGenException
+{
+
+    public function __construct($message, $code=0)
+    {
+        parent::__construct($message, $code);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+} /* end class CertificateQueryException */
+?>
