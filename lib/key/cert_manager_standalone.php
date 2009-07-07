05fc7401b6d3e3631e88fbdd3bd75c7df89b8be0
Henrik Austad <henrik.austad@uninett.no>
2009-07-07 14:46:36 +0200

    cert_manager: match_dn only used by standalone
    the match_dn() is only used in cert_manager_standalone and should be
    kept in private there.
    
    Signed-off-by: Henrik Austad <henrik.austad@uninett.no>

--- lib/key/cert_manager_standalone.php
+++ lib/key/cert_manager_standalone.php
@@ -166,6 +166,33 @@ class CertManager_Standalone extends CertManager
 	       return true;
     } /* end verify_csr */
 
+  /* match_dn
+   *
+   * This will match the associative array $subject with the constructed DN from person->get_complete_dn()
+   *
+   * The best would be to use something like what openssl supports:
+   *	openssl x509 -in usercert.pem -subject -noout
+   * which returns the subject string as we construct it below. However,
+   * php5_openssl has no obvious way of doing that.
+   *
+   * Eventually, we have to add severeal extra fields to handle all different
+   * cases, but for now, this will do.
+   */
+  private function match_dn($subject)
+  {
+	  /* Compose the DN in the 'correct' order, only use the fields set in
+	   * the subject */
+	  $composed_dn = "";
+	  if (isset($subject['C']))
+		  $composed_dn .= "/C=".$subject['C'];
+	  if (isset($subject['O']))
+		  $composed_dn .= "/O=".$subject['O'];
+	  if (isset($subject['OU']))
+		  $composed_dn .= "/OU=".$subject['OU'];
+	  if (isset($subject['C']))
+		  $composed_dn .= "/CN=".$subject['CN'];
+	  return $this->person->get_complete_dn() === $composed_dn;
+  }
 
 } /* end class CertManager_Standalone */
 ?>
