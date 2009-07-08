a12978bc306ceca99a98dca4efb81c7ee95f66d8
Henrik Austad <henrik.austad@uninett.no>
2009-07-08 15:00:13 +0200

    output: add message-cap. to db_array_debug()
    Signed-off-by: Henrik Austad <henrik.austad@uninett.no>

--- lib/misc/output.php
+++ lib/misc/output.php
@@ -5,9 +5,11 @@ function error_output($msg) {
 	echo "</B></FONT>\n";
   }
 
-function db_array_debug($array)
+function db_array_debug($array, $msg=null)
 {
 	if (Config::get_config('debug') && count($array) > 1) {
+		if (isset($msg))
+			echo $msg . "<BR>\n";
 		echo "<PRE>\n";
 		print_r($array);
 		echo "</PRE>\n";
