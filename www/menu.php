f66b02b9e128dba8662c98f09544353576fa7512
Henrik Austad <henrik.austad@uninett.no>
2009-07-08 13:12:46 +0200

    menu: use get_status() to determine the type of menu to show to the user
    Signed-off-by: Henrik Austad <henrik.austad@uninett.no>

--- www/menu.php
+++ www/menu.php
@@ -5,11 +5,36 @@ function render_menu($person)
     /* always show the Frontpage-link (this page should always be
      * visible */
     if ($person->is_auth()) {
-	    echo get_menu_name("process_csr.php",		"Request New Certificate");
-	    echo get_menu_name("download_certificate.php",	"Download Certificate");
-	    echo get_menu_name("revoke_certificate.php",	"Revoke Certificate");
-	    echo get_menu_name("about_nren.php",		"About <NREN>");
-	    echo get_menu_name("help.php",			"Help <NREN>");
+	    /* use get_mode to figure out the mode. Note: non-admin users will
+	     * have NORMAL_MODE returned regardless of datbase etc. */
+	    $mode = $person->get_mode();
+	    if ($mode == NORMAL_MODE) {
+		    echo get_menu_name("process_csr.php",		"Request New Certificate");
+		    echo get_menu_name("download_certificate.php",	"Download Certificate");
+		    echo get_menu_name("revoke_certificate.php",	"Revoke Certificate");
+		    echo get_menu_name("about_nren.php",		"About <NREN>");
+		    echo get_menu_name("help.php",			"Help <NREN>");
+		    if ($person->is_admin()) {
+			    echo get_menu_name("index.php?mode=admin", "Admininstrative menu");
+		    }
+	    } else if ($mode == ADMIN_MODE) {
+		    /* Create the admin-menu based on admin privileges.
+		     * The pages common for for more than one type of admin (or
+		     * normal users) will also check privileges.
+		     */
+		    if ($person->is_subscriber_sub_admin()) {
+			    echo get_menu_name("revoke_cert.php",	"Revoke Certificates");
+		    } else if ($person->is_subscriber_sub_admin()) {
+			    echo get_menu_name("revoke_cert.php",	"Revoke Certificates");
+			    echo get_menu_name("admin.php",		"Manage Subscriber Administrators");
+			    echo get_menu_name("robot.php",		"Robot Interface");
+		    } else if ($person->is_nren_admin()) {
+			    echo get_menu_name("admin.php",		"Manage Administrators");
+		    }
+		    echo get_menu_name("index.php?mode=normal",	"Normal mode");
+	    }
+
+	    echo "END NEW MENU<BR>\n";
 	    echo "<BR><BR>\n";
 	    echo get_menu_name("index.php", "Start");
 	    echo get_menu_name("tools.php","Tools");
