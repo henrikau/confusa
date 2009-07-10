d341c4e6b2f37f8a7c9b715852b963c067f32250
Henrik Austad <henrik.austad@uninett.no>
2009-07-10 10:14:39 +0200

    header: added favicon to header section
    Signed-off-by: Henrik Austad <henrik.austad@uninett.no>

--- include/header.php
+++ include/header.php
@@ -2,6 +2,7 @@
 <HEAD>
 <TITLE>Confusa</TITLE>
 <LINK REL="stylesheet" TYPE="text/css" HREF="confusa.css">
+<LINK REL="shortcut icon" HREF="graphics/icon.gif" TYPE="image/gif"/>
 </HEAD>
 <BODY>
 <!-- formatting of system-logo head -->
