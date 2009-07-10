fa207f5896d880e5e44bf58da38c7657e0bdb52e
Henrik Austad <henrik@austad.us>
2009-07-10 13:34:37 +0200

    index: simplify logic by using csr_lib functionality
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- www/index.php
+++ www/index.php
@@ -156,31 +156,28 @@ function approve_csr($auth_token)
 {
      global $person;
      $status = false;
-     $csr_res = MDB2Wrapper::execute("SELECT csr FROM csr_cache WHERE auth_key=? AND common_name=?",
-                                     array('text', 'text'),
-                                     array($auth_token, $person->get_valid_cn()));
-     if (count($csr_res) == 1) {
-          $csr = $csr_res[0]['csr'];
-	  $cm = CertManagerHandler::getManager($person);
-	  if (Config::get_config('debug')) {
+     try {
+	     $csr = get_csr_from_db($person, $auth_token);
+     } catch (CSRNotFoundException $csrnfe) {
+	     return false;
+     } catch (ConfusaGenException $cge) {
+	     /* log this? */
+	     return false;
+     }
+     $cm = CertManagerHandler::getManager($person);
+     if (Config::get_config('debug')) {
 		  echo "Content of CSR:<BR>\n";
 		  echo "<PRE>\n";
 		  echo $csr;
 		  echo "</PRE>\n";
-	  }
-          try {
-            $cm->sign_key($auth_token, $csr);
-          } catch (ConfusaGenException $e) {
-               echo __FILE__ .":".__LINE__." Error signing key<BR>\n";
-               return false;
-          }
-	  MDB2Wrapper::update("DELETE FROM csr_cache WHERE auth_key=? AND common_name=?",
-			      array('text', 'text'),
-			      array($auth_token, $person->get_valid_cn()));
-	  return true;
      }
-     echo __FILE__ .":".__LINE__." error getting CSR from database<BR>\n";
-     return false;
+     try {
+	     $cm->sign_key($auth_token, $csr);
+     } catch (ConfusaGenException $e) {
+	     echo __FILE__ .":".__LINE__." Error signing key<BR>\n";
+	     return false;
+     }
+     return delete_csr_from_db($person, $auth_token);
 } /* end approve_csr_remote() */
 
 /* send_cert
