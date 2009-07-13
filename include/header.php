8361ed8f7b4d1d59dcbbb2d9db2c25bc211a6a47
Henrik Austad <henrik@austad.us>
2009-07-13 15:37:33 +0200

    header: make header.php more dynamic so we can rewrite headers at will
    The new framework may need to rewrite the headers and still present the
    same page as before. This is an attempt to set refresh and target
    dynamically without hijacking the entire header.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- include/header.php
+++ include/header.php
@@ -1,9 +1,25 @@
-<HTML>
-<HEAD>
-<TITLE>Confusa</TITLE>
-<LINK REL="stylesheet" TYPE="text/css" HREF="confusa.css">
-<LINK REL="shortcut icon" HREF="graphics/icon.gif" TYPE="image/gif"/>
-</HEAD>
-<BODY>
-<!-- formatting of system-logo head -->
+<?php
 
+$title = "Confusa";
+$extra_header = "";
+
+function set_title($new_title)
+{
+	$title = $new_title;
+}
+
+function add_header($extra_header)
+{
+
+}
+function show_headers()
+{
+	echo "<HTML>\n";
+	echo "<HEAD>\n";
+	echo "$extra_header\n";
+	echo "<TITLE>$title</TITLE>\n";
+	echo "<LINK REL=\"stylesheet\" TYPE=\"text/css\" HREF=\"confusa.css\">\n";
+	echo "<LINK REL=\"shortcut icon\" HREF=\"graphics/icon.gif\" TYPE=\"image/gif\"/>\n";
+	echo "</HEAD>\n";
+}
+?>
