8ecba56543f46c5024243931f1ae40f7d02aeb3d
Henrik Austad <henrik@austad.us>
2009-07-14 11:05:05 +0200

    output: use the new message-site to display information
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- lib/misc/output.php
+++ lib/misc/output.php
@@ -32,21 +32,17 @@ function format_ip($ip, $show_help=false)
 		$pre =  "<FONT COLOR=\"RED\"><B><I>";
 		$post = "</I></B></FONT>";
 		if ($show_help) {
-			$msg  = "The address of the machine uploading a CSR is recorded. ";
-			$msg .= "If, for any reason, the address of your current machine differs from that, a notification ";
-			$msg .= "is displayed. This may, or may not, mean anything, but you should be aware of this. ";
-			$msg .= "Your current IP is " . $ip;
-			$help = " [ " . show_window("?", $msg) . " ] ";
+			$help = " [ " . show_window("?", "messages/diff_ip.php") . " ] ";
 		}
 	}
 	return "$pre$ip$post$help";
 
 }
 
-function show_window($url_name, $message)
+function show_window($url_name, $target)
 {
 	$help  = " <A HREF=\"\"";
-	$help .= "onClick =\"window.open('" . $message . "', '', 'width=400,height=300');\"";
+	$help .= "onClick =\"window.open('" . $target . "', '', 'width=500,height=400');\"";
 	$help .= ">";
 	$help .=  $url_name . "</A> \n";
 	return $help;
