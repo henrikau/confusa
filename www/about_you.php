b60d1bf10520f5bce3df81a4b793249df30e09e9
Henrik Austad <henrik@austad.us>
2009-07-15 11:35:24 +0200

    Port about_you ContentPage setup
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- www/about_you.php
+++ www/about_you.php
@@ -1,30 +1,47 @@
 <?php
-require_once('confusa_include.php');
-include_once('framework.php');
+require_once 'confusa_include.php';
+require_once 'framework.php';
 
-$fw = new Framework('about_you');
-$fw->force_login();
-$fw->render_page();
-
-function about_you($person) {
-	$textual = "no";
-
-	if (isset($_GET['text'])) {
-		$textual = htmlentities($_GET['text']);
+class AboutYou extends ContentPage
+{
+	function __construct()
+	{
+		parent::__construct("About You", true);
+		$this->signing_ok = false;
 	}
 
-	if ($textual == "yes") {
-		/* The id makes it very easy to automatically retrieve the content, e.g. with XPath */
-		echo "<DIV ID=\"dn-section\">";
-		echo $person->get_complete_dn();
-		echo "</DIV>";
-	} else {
-		echo "<H3>This is what we know about you:</H3>\n";
-		echo $person;
-		echo "<HR>\n";
-		echo "we store very little information. What we do keep, is information about certificates issued, combinded with the eduPersonPrincipalName\n";
-		echo "This is the DN in the certificate, and whe <b>have</b> to store this.<BR>\n";
+	public function pre_process($person)
+	{
+		$this->setPerson($person);
+		$this->setManager();
+		return false;
 	}
-	
+	public function process($person)
+	{
+		$textual = "no";
+		if (isset($_GET['text'])) {
+			$textual = htmlentities($_GET['text']);
+		}
+		if ($textual == "yes") {
+			/* The id makes it very easy to automatically retrieve the content, e.g. with XPath */
+			echo "<DIV ID=\"dn-section\">";
+			echo $person->get_complete_dn();
+			echo "</DIV>";
+		} else {
+			echo "<H3>This is what we know about you:</H3>\n";
+			echo $person;
+			echo "<HR>\n";
+			echo "we store very little information. What we do keep, is information about certificates issued, combinded with the eduPersonPrincipalName\n";
+			echo "This is the DN in the certificate, and whe <b>have</b> to store this.<BR>\n";
+		}
+	}
+	public function post_render($person)
+	{
+		/* cleanups etc? */
+	}
 }
+
+$fw = new Framework(new AboutYou());
+$fw->start();
+
 ?>
