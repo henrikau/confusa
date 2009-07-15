fa5ed40631ee833d3c6a346bdb970dee0ec597ed
Henrik Austad <henrik@austad.us>
2009-07-15 11:51:57 +0200

    port About_NREN to ContentPage
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- www/about_nren.php
+++ www/about_nren.php
@@ -3,27 +3,47 @@ require_once 'confusa_include.php';
 require_once 'framework.php';
 require_once 'person.php';
 
-$fw = new Framework('about_nren');
-$fw->render_page();
-
-function about_nren($person)
+class About_NREN extends ContentPage
 {
-	echo "<H3>NREN Area</H3>\n";
+	function __construct()
+	{
+		parent::__construct("About NREN", false);
+	}
 
-	if ($person->is_auth())
-		auth_page($person);
-	else
-		open_page();
-}
+	public function pre_process($person)
+	{
+		$this->setPerson($person);
+		$this->setManager();
+		return false;
+	}
 
-function auth_page($page)
-{
-	echo "The classified stuff..<BR />\n";
-}
+	public function process($person)
+	{
+		echo "<H3>NREN Area</H3>\n";
 
-function open_page()
-{
-	  include('unclassified_intro.php');
-}
+		if ($person->is_auth()) {
+			$this->auth_page($person);
+		} else {
+			$this->open_page();
+		}
+	}
+
+	public function post_render($person)
+	{
+		return ;
+	}
+	private function auth_page($page)
+	{
+		echo "The classified stuff..<BR />\n";
+	}
+
+	private function open_page()
+	{
+		include('unclassified_intro.php');
+	}
+}	
+
+$fw = new Framework(new About_NREN());
+$fw->start();
 
 ?>
