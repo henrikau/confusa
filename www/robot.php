10f823330e89802ed7afe3379b1c09666452dd2f
Henrik Austad <henrik@austad.us>
2009-07-15 12:38:30 +0200

    Robot: add robot-interface for admins.
    To manage the robotic interface, a menu-entry should be given for the
    administrators so they can register certificates for the automated
    tools.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

---
+++ www/robot.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'confusa_include.php';
+require_once 'framework.php';
+require_once 'person.php';
+
+class Robot_Interface extends ContentPage
+{
+	function __construct()
+	{
+		parent::__construct("Robot", true);
+	}
+
+	public function pre_process($person)
+	{
+		$this->setPerson($person);
+		$this->setManager();
+		return false;
+	}
+
+	public function process($person)
+	{
+		echo "<H3>Robot Interface</H3>\n";
+		echo "This is where you administer the robotic interface for your institution<BR />\n";
+	}
+
+	public function post_render($person)
+	{
+		return ;
+	}
+}
+
+$fw = new Framework(new Robot_Interface());
+$fw->start();
+
+?>
