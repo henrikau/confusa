76ea792c49eb488b57a77cfbd7b7675f49c3906a
Henrik Austad <henrik@austad.us>
2009-07-15 13:10:11 +0200

    port tools to ContentPage
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- www/tools.php
+++ www/tools.php
@@ -4,49 +4,66 @@ include_once('framework.php');
 include_once('mail_manager.php');
 include_once('logger.php');
 
-$fw = new Framework('tools');
-$fw->force_login();
-
-/* test to see ifthe user wants to receive tools via direct file-download or email */
-test_send_tools();
-$fw->render_page();
-function tools($person)
+class Tools extends ContentPage
 {
-     include('tools.html');
-} /* end tools */
+	public function __construct()
+	{
+		parent::__construct("Revoke Certificate(s)", true);
+	}
 
-function test_send_tools()
-{
-     global $person;
-     global $fw;
-     if (!( isset($person) && $person->is_auth()  ))
-          $person = $fw->authenticate();
+	function __destruct()
+	{
+		parent::__destruct();
+	}
+	public function pre_process($person)
+	{
+		$this->setPerson($person);
+		$this->setManager();
+		if (isset($_GET['send_file'])) {
+			include_once('file_download.php');
+			include_once('create_keyscript.php');
+			$keyscript = new KeyScript($person);
+			download_file($keyscript->create_script(), "create_cert.sh");
+			Logger::log_event(LOG_NOTICE, "Sending script via file to ". $person->get_common_name());
+			exit(0);
+		}
+		return false;
+	}
+	public function process($person)
+	{
+		echo "<H3>Certificate Revocation Area</H3>\n";
 
-     if (isset($_GET['send_email'])) {
-          include_once('create_keyscript.php');
-          $keyscript = new KeyScript($person);
-          $eol = "\r\n";
-          $body = "";
-          $body .= "Attached is a custom-designed script for creating keys" . $eol;
-          $body .= "Save script to computer, set executable (chmod u+x create_cert.sh) and run" . $eol;
-          $body .= "The script will prompt for a passphrase for the key. Read the instructions carefully!" . $eol;
-          $subject = 'Script for creating key and certificate request for ARC';
-          $mail = new MailManager($person,
-                                  Config::get_config('sys_from_address'),
-                                  $subject,
-                                  $body);
-          $mail->add_attachment($keyscript->create_script(), "create_cert.sh");
-          $mail->send_mail();
-          echo "<i><b><center>Mail sent to " . $person->get_email() . " with new version of create_cert.sh</center></b></i><br>\n";
-     }
-     if (isset($_GET['send_file'])) {
-          include_once('file_download.php');
-          include_once('create_keyscript.php');
-          $keyscript = new KeyScript($person);
-          download_file($keyscript->create_script(), "create_cert.sh");
-          Logger::log_event(LOG_NOTICE, "Sending script via file to ". $person->get_common_name());
-          exit(1);
-     }
+		include('tools.html');
 
-} /* end test_send_tools */
+		if (isset($_GET['send_email']))
+			$this->send_email();
+	}
+
+	public function post_render($person)
+	{
+		return;
+	}
+	private function send_email()
+	{
+		include_once('create_keyscript.php');
+		$keyscript = new KeyScript($this->person);
+		$eol = "\r\n";
+		$body = "";
+		$body .= "Attached is a custom-designed script for creating keys" . $eol;
+		$body .= "Save script to computer, set executable (chmod u+x create_cert.sh) and run" . $eol;
+		$body .= "The script will prompt for a passphrase for the key. Read the instructions carefully!" . $eol;
+		$subject = 'Script for creating key and certificate request for ARC';
+		$mail = new MailManager($this->person,
+					Config::get_config('sys_from_address'),
+					$subject,
+					$body);
+		$mail->add_attachment($keyscript->create_script(), "create_cert.sh");
+		$mail->send_mail();
+		echo "<I>Mail sent to " . $this->person->get_email() . " with new version of create_cert.sh</I><BR />\n";
+
+	}
+}
+$fw = new Framework(new Tools());
+$fw->start();
+
 ?>
