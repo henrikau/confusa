8bb40e13b01c305c7b5ecd7f4bdd1095b1a68c5f
Henrik Austad <henrik@austad.us>
2009-07-16 10:31:07 +0200

    upload_form: make the upload-form more generic
    We need to handle other uploads than CSRs, so make the template-form
    more generic
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- include/upload_form.php
+++ include/upload_form.php
@@ -1,8 +1,21 @@
 <?php
-function show_upload_form($target)
+  /**
+   * show_upload_form - create an upload form for arbitrary data.
+   *
+   * @target_url  : the page that will handle the upload
+   * @input_name  : the name of the field where the data is stored
+   * @submit_name : If the name of the submit-button (upload) should be
+   *		    changed.
+   */
+function show_upload_form($target_url, $input_name, $submit_name = NULL)
 {
 	if (!isset($target))
 		return;
+	if (!isset($input_name))
+		return;
+	$sname = "Upload";
+	if (isset($submit_name))
+		$sname = $submit_name;
 ?>
 <FORM ENCTYPE="multipart/form-data" ACTION="<?php echo $target ?>" METHOD="POST">
   <INPUT TYPE="hidden" 
@@ -10,8 +23,8 @@ function show_upload_form($target)
 	 VALUE="2000000" />
 Please upload your CSR for signing. Signed certificates will be stored in the database.
     <BR />
-    <INPUT NAME="user_csr" TYPE="file" />
-  <INPUT TYPE="submit" VALUE="Upload CSR" />
+    <INPUT NAME="<?php echo $input_name ?>" TYPE="file" />
+  <INPUT TYPE="submit" VALUE="echo $sname ?>" />
 </FORM>
 
 <?php
