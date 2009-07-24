9a4ca610a90263279d063c5760b514c4e90b2e98
Erik Smistad <smistad@idi.ntnu.no>
2009-07-24 14:40:56 +0200

    Framework: Move framework to template-system
    This is a large hunk,and inbetween is the new error-output function.
    This has been removed from error_output.

--- lib/framework/framework.php
+++ lib/framework/framework.php
@@ -1,11 +1,11 @@
 <?php
 
-  /* framework.php
-   *
-   * Framework class for Confusa.
-   *
-   * This will handle all aspects regarding layout and authentication of user.
-   */
+/* framework.php
+ *
+ * Framework class for Confusa.
+ *
+ * This will handle all aspects regarding layout and authentication of user.
+ */
 require_once 'confusa_include.php';
 require_once 'confusa_auth.php';
 require_once 'menu.php';
@@ -13,7 +13,6 @@ require_once 'person.php';
 require_once 'logger.php';
 require_once 'content_page.php';
 require_once 'output.php';
-require_once 'header.php';
 
 /* global config */
 require_once 'config.php';
@@ -36,111 +35,98 @@ require_once '/usr/share/php/smarty/Smarty.class.php';
 class Framework {
 	private $person;
 	private $contentPage;
+	private $tpl;
+	private static $errors = array();
 
-    public function __construct($contentPage) {
-	    if (!isset($contentPage)) {
-		    error_output("Error! content_page not provided to Framework constructor");
-		    exit(0);
-	    }
-	    if (!($contentPage instanceof FW_Content_Page)) {
-		    error_output("Supplied contentPage is not of class FW_Content_Page");
-		    exit(0);
-	    }
-	    if (!Config::get_config('valid_install')) {
-		    echo "You do not have a valid configuration. Please edit the confusa_config.php properly first<BR>\n";
-		    exit(0);
-	    }
-	    $this->contentPage = $contentPage;
-	    $this->person = new Person();
-	    set_title($this->contentPage->get_title());
-    }
-
-    public function authenticate() {
-        is_authenticated($this->person);
-        if (!$this->person->is_auth()) {
-		/* if login, trigger SAML-redirect first */
-		if ($this->contentPage->is_protected() || (isset($_GET['start_login']) && $_GET['start_login'] === 'yes')) {
-			_assert_sso($this->person);
+	public function __construct($contentPage) {
+		if (!isset($contentPage)) {
+			Framework::error_output("Error! content_page not provided to Framework constructor");
+			exit(0);
 		}
-        }
-	$uname = "anonymous";
-	if($this->person->is_auth())
+		if (!($contentPage instanceof FW_Content_Page)) {
+			Framework::error_output("Supplied contentPage is not of class FW_Content_Page");
+			exit(0);
+		}
+		if (!Config::get_config('valid_install')) {
+			echo "You do not have a valid configuration. Please edit the confusa_config.php properly first<BR>\n";
+			exit(0);
+		}
+		$this->contentPage = $contentPage;
+		$this->person = new Person();
+		$this->tpl = new Smarty();
+		$this->tpl->template_dir = Config::get_config('install_path').'lib/smarty/templates';
+		$this->tpl->compile_dir = Config::get_config('install_path').'lib/smarty/templates_c';
+		$this->tpl->config_dir = Config::get_config('install_path').'lib/smarty/configs';
+		$this->tpl->cache_dir = Config::get_config('install_path').'lib/smarty/cache';
+		$this->tpl->assign('title', Config::get_config('system_name').' '.$this->contentPage->get_title());
+	}
+
+	public function authenticate() {
+		is_authenticated($this->person);
+		if (!$this->person->is_auth()) {
+			/* if login, trigger SAML-redirect first */
+			if ($this->contentPage->is_protected() || (isset($_GET['start_login']) && $_GET['start_login'] === 'yes')) {
+				_assert_sso($this->person);
+			}
+		}
+		$uname = "anonymous";
+		if($this->person->is_auth())
 		$uname = $this->person->get_valid_cn();
-        return $this->person;
-    }
+		return $this->person;
+	}
 
-   public function start()
-   {
-	   /* check the authentication-thing, catch the login-hook
-	    * This is done via confusa_auth
-	    */
-	   $this->authenticate();
+	public function start()
+	{
+		/* check the authentication-thing, catch the login-hook
+		 * This is done via confusa_auth
+		 */
+		$this->authenticate();
+		
+		/* Set tpl object to content page */
+		$this->contentPage->setTpl($this->tpl);
 
-	   /* Allow content-page to do pre-process */
-	   $res = $this->contentPage->pre_process($this->person);
-	   if ($res) {
-		   add_header($res);
-	   }
-	   show_headers();
-	   echo "<BODY>\n";
-	   echo "<CENTER>\n";
-	   echo "<TABLE class=\"header\">\n";
-	   echo "<TR>\n";
-	   echo "<TD ><IMG SRC=\"graphics/logo-sigma.png\"></TD>\n";
-	   echo "<TD WIDTH=\"100\"></TD>\n";
-	   echo "<TD><FONT SIZE=\"+2\">Confusa</TD>\n";
-	   echo "</TR>\n";
-	   echo "</TABLE>\n";
-	   echo "</CENTER>\n";
+		/* Allow content-page to do pre-process */
+		$res = $this->contentPage->pre_process($this->person);
+		if ($res) {
+			$this->tpl->assign('extraHeader', $res);
+		}
 
-	 /* Mode-hook, to catch mode-change regardless of target-page (not only
-	  * index) */
-	 if (isset($_GET['mode'])) {
-		 $new_mode = NORMAL_MODE;
-		 if (htmlentities($_GET['mode']) == 'admin') {
-			 $new_mode = ADMIN_MODE;
-		 }
-		 $this->person->set_mode($new_mode);
-	 }
+		/* Mode-hook, to catch mode-change regardless of target-page (not only
+		 * index) */
+		if (isset($_GET['mode'])) {
+			$new_mode = NORMAL_MODE;
+			if (htmlentities($_GET['mode']) == 'admin') {
+				$new_mode = ADMIN_MODE;
+			}
+			$this->person->set_mode($new_mode);
+		}
+		$this->tpl->assign('person', $this->person);
+		$this->contentPage->process($this->person);
+		$this->tpl->assign('menu', $this->tpl->fetch('menu.tpl')); // see render_menu($this->person)
+		$this->tpl->assign('errors', self::$errors);
+		$this->tpl->display('site.tpl');
+		
+		$this->contentPage->post_process($this->person);
+		
+	} /* end render_page */
 
+	private function user_rendering()
+	{
+		/* check to see if the user wants to log in, if so, start login-procedure */
+		if (!$this->person->is_auth()) {
+			if ($this->flogin || (isset($_GET['start_login']) && $_GET['start_login'] === 'yes')) {
+				authenticate_user($this->person);
+			}
+			if (isset($_POST['start_login']) && $_POST['start_login'] == 'yes')
+			authenticate_user($this->person);
+		}
+		$func = $this->f_content;
+		$func($this->person);
+	} /* end user-rendering */
+
+	public static function error_output($message)
+	{
+		self::$errors[] = $message;
+	}
 	
-        echo "\n<TABLE class=\"main\">\n";
-        echo "\t<TR>\n";
-
-        /* === MENU === */
-        echo "\t\t<TD class=\"main\" WIDTH=\"100\" VALIGN=\"TOP\">\n";
-        render_menu($this->person);
-        echo "\t\t</TD>\n";
-
-	/* === Main === */
-        echo "\t\t<TD class=\"main\">\n";
-	echo "\t\t<TABLE class=\"content\">\n";
-	echo "\t\t\t<TR><TD>\n";
-	$this->contentPage->process($this->person);
-	echo "\t\t\t</TD></TR>\n";
-	echo "\t\t</TABLE>\n";
-        echo "\t\t</TD>\n";
-
-	/* === Post-amble */
-        echo "</TABLE>\n";
-        include_once 'footer.php';
-
-
-	$this->contentPage->post_process($this->person);
-        } /* end render_page */
-
-    private function user_rendering()
-        {
-        /* check to see if the user wants to log in, if so, start login-procedure */
-            if (!$this->person->is_auth()) {
-                if ($this->flogin || (isset($_GET['start_login']) && $_GET['start_login'] === 'yes')) {
-                    authenticate_user($this->person);
-                    }
-            if (isset($_POST['start_login']) && $_POST['start_login'] == 'yes')
-                authenticate_user($this->person);
-            }
-            $func = $this->f_content;
-            $func($this->person);
-        } /* end user-rendering */
-
-    } /* end class Framewokr */
+} /* end class Framewokr */
