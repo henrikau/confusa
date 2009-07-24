7efbab42d39715219b8aaba22f75644a45f077c2
Henrik Austad <henrik@austad.us>
2009-07-24 15:40:45 +0200

    use templates in about_you
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- www/about_you.php
+++ www/about_you.php
@@ -15,18 +15,8 @@ class AboutYou extends FW_Content_Page
 		if (isset($_GET['text'])) {
 			$textual = htmlentities($_GET['text']);
 		}
-		if ($textual == "yes") {
-			/* The id makes it very easy to automatically retrieve the content, e.g. with XPath */
-			echo "<DIV ID=\"dn-section\">";
-			echo $this->person->get_complete_dn();
-			echo "</DIV>";
-		} else {
-			echo "<H3>This is what we know about you:</H3>\n";
-			echo $this->person;
-			echo "<HR>\n";
-			echo "we store very little information. What we do keep, is information about certificates issued, combinded with the eduPersonPrincipalName\n";
-			echo "This is the DN in the certificate, and whe <b>have</b> to store this.<BR>\n";
-		}
+		$this->tpl->assign('textual', $textual);
+		$this->tpl->assign('content', $this->tpl->fetch('about_you.tpl'));
 	}
 }
 
