c345e353cb6ff274ade10819de142823dfc9eab4
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-07-28 14:55:19 +0200

    Update view definition
    Reflect PK-changes in DB schema. Add additional columns to
    nren_account_map_view to avoid joins in Confusa.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- init/views_create.sql
+++ init/views_create.sql
@@ -1,6 +1,7 @@
 -- ---------------------------------------------------------
 --
--- Make cleaner way
+-- Create views in order not having to join every time across frequently
+-- combined tables in Confusa
 --
 -- ---------------------------------------------------------
 CREATE OR REPLACE ALGORITHM = TEMPTABLE
@@ -11,23 +12,25 @@ VIEW nren_subscriber_view
 AS SELECT
 	s.name,
 	s.org_state,
-	n.name 
+	n.name
 FROM
 	subscribers s
 LEFT JOIN
 	nrens n
 ON
-	s.nren_name = n.name;
+	s.nren_id = n.nren_id;
 
 CREATE OR REPLACE ALGORITHM = TEMPTABLE
-VIEW nrens_account_map_view
-	(nren_name, account_login_name)
+VIEW nren_account_map_view
+	(nren, account_login_name, account_password, account_ivector)
 AS SELECT
-	nrens.name,
-	nrens.login_name
+	n.name,
+	a.login_name,
+	a.password,
+	a.ivector
 FROM
-	nrens
+	nrens n
 LEFT JOIN
-	account_map
+	account_map a
 ON
-	nrens.login_name = account_map.login_name;
+	n.login_account = a.account_map_id;
