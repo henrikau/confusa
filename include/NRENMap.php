bd44607956a172730caac88c912815c33e92f101
Henrik Austad <henrik@austad.us>
2009-07-30 11:03:53 +0200

    NREN: move mapping of IdP -> NREN into dedicated Map.
    When finding the appropriate NREN, the cleanest solution is to add this
    in a dedicated map, and add that to the list of modules to load by SimpleSAMLphp.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

---
+++ include/NRENMap.php
@@ -0,0 +1,30 @@
+<?php
+  /* Henrik Austad, 2009
+   *
+   * Part of Confusa, GPLv3 applies.
+   *
+   * This maps an IdP to an NREN. As an IdP must belong to one and only one NREN,
+   * but one NREN may consist of several IdPs, we must map this the same way we
+   * map the Countries.
+   */
+class sspmod_core_Auth_Process_NRENMap extends SimpleSAML_Auth_ProcessingFilter {
+	private $known_nrens = array();
+
+	public function __construct($config, $reserved) {
+		parent::__construct($config, $reserved);
+		/* set the known idps */
+		$this->known_nrens = array('https://openidp.feide.no'		=> 'uninett',
+					  'max.feide.no'			=> 'uninett',
+					  'edugain.showcase.surfnet.nl'		=> 'sufnet',
+					  'https://testidp.wayf.dk'		=> 'wayf',
+					  'https://betawayf.wayf.dk'		=> 'wayf',
+					  'https://aitta2.funet.fi/idp/shibboleth'	=> 'haka'
+			);
+	}
+
+	public function process(&$request) {
+		$idp = $request['Source']['entityid'];
+		$request['Attributes']['nren'] = array($this->known_nrens[$idp]);
+	}
+  }
+?>
