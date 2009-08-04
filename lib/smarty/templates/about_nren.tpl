0bcc4d530d736f1ce41ef0f4c42a56b4a0d60cfb
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-08-04 15:21:38 +0200

    Add display of NREN-customized about messages
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- lib/smarty/templates/about_nren.tpl
+++ lib/smarty/templates/about_nren.tpl
@@ -1,6 +1,17 @@
 <h2>About NREN</h2>
 {if $person->is_auth()}
-The classified stuff...
+<div class="spacer"></div>
+
+{if !is_null($logo)}
+	<div id="logo_in_text">
+		<img src={$logo} alt="NREN logo"</img>
+	</div>
+{/if}
+
+{if !is_null($text_info)}
+	{$text_info}
+{/if}
+
 {else}
 {include file='unclassified_intro.tpl'}
 {/if}
--- www/about_nren.php
+++ www/about_nren.php
@@ -13,10 +13,43 @@ class About_NREN extends FW_Content_Page
 
 	public function process()
 	{
+		$logo = Framework::get_logo_for_nren($this->person->get_nren());
+		$this->tpl->assign('logo', $logo);
+		$about_text = $this->getAboutTextForNREN($this->person->get_nren());
+		$this->tpl->assign('text_info', $about_text);
 		$this->tpl->assign('content', $this->tpl->fetch('about_nren.tpl'));
 	}
 
-}	
+	/*
+	 * Get the about-text for a certain NREN, so it can be displayed in Confusa's
+	 * about-section
+	 */
+	private function getAboutTextForNREN($nren)
+	{
+		$query = "SELECT about FROM nrens WHERE name = ?";
+
+		try {
+			$res = MDB2Wrapper::execute($query,
+										array('text'),
+										array($nren));
+		} catch (DBStatementException $dbse) {
+			Framework::error_output("Error fetching the NREN about-page. Probably a " .
+									"configuration problem! Server said: " . $dbse->getMessage());
+			return "";
+		} catch (DBQueryException $dbqe) {
+			Framework::error_output("Error fetching the NREN about-page. Looks like a " .
+									"problem with the supplied data. Queried NREN was " . $nren);
+			return "";
+		}
+
+		if (count($res) > 0) {
+			return $res[0]['about'];
+		} else {
+			return "";
+		}
+	}
+
+}
 
 $fw = new Framework(new About_NREN());
 $fw->start();
