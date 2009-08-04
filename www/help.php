17890049ee690d2e923d941009680187fc66f3e1
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-08-04 15:16:16 +0200

    Add support for displaying NREN specific help messages
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- www/help.php
+++ www/help.php
@@ -12,10 +12,43 @@ class Help extends FW_Content_Page
 
 	public function process()
 	{
-		
+		$help_text = $this->getNRENHelpText($this->person->get_nren());
+		$this->tpl->assign('nren_help_text', $help_text);
 		$this->tpl->assign('help_file', file_get_contents('../include/ipso_lorem.html'));
 		$this->tpl->assign('content', $this->tpl->fetch('help.tpl'));
 
+	}
+
+	/*
+	 * Get the custom help text entered for/by a certain NREN
+	 *
+	 * @param $nren The NREN for which the help-text should be retrieved
+	 */
+	private function getNRENHelpText($nren)
+	{
+		$query = "SELECT help FROM nrens WHERE name = ?";
+
+		$res = array();
+
+		try {
+			$res = MDB2Wrapper::execute($query,
+										array('text'),
+										array($nren));
+		} catch (DBStatementException $dbse) {
+			Framework::error_output("Could not retrieve the help text of your NREN due " .
+									"to an error with the statement. Server said " .
+									$dbse->getMessage());
+			return "";
+		} catch (DBQueryException $dbqe) {
+			Framework::error_output("Could not retrieve the help text of your NREN due " .
+									"to an error in the query. Server said " .
+									$dbqe->getMessage());
+			return "";
+		}
+
+		if (count($res) > 0) {
+			return $res[0]['help'];
+		}
 	}
 }
 
