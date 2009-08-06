ca35e57536bac9540f112e6757431a626af810a4
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-08-06 14:08:45 +0200

    Sanitize about and help texts more reasonably
    The NREN about/help texts should be able to display characters like ' (for
    names, grammatical constructions like it's), ",$,€,£ and whatnot.
    Furthermore, all sorts of unicode characters should be displayable.
    preg_replace doesn't make sense in that scenario, just use htmlentities
    plus mysql_real_escape_string. The solution has been tested against SQL
    injection and XSS.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- lib/misc/input.php
+++ lib/misc/input.php
@@ -26,5 +26,42 @@ class Input
 		$output = preg_replace('/[^a-z0-9_.@ ]+/i','',$input);
 		return $output;
 	}
+
+	/*
+	 * For text e.g. defined by the NREN admin to view on the help/about page
+	 * we can not make too many assumptions about how the input will look like
+	 * It must be possible to represent both special characters like åäö and
+	 * characters like ' in the floating text.
+	 *
+	 * That's why we just try to get rid of SQL-injection-ish characters as well
+	 * as HTML characters and leave the rest of the text untouched.
+	 *
+	 * @param $input the text which we want to sanitize
+	 */
+	static function sanitizeText($input)
+	{
+		if (!isset($input) || empty($input)) {
+			return null;
+		}
+
+		if (is_array($input)) {
+			foreach($input as $var=>$val) {
+				$output[$var] = Input::sanitizeText($val);
+			}
+		}
+
+		$input = htmlentities($input, ENT_QUOTES, 'UTF-8');
+		/* remove paragraphs */
+		$input = preg_replace('/(\r|\n)+/','', $input);
+
+		if (ini_get("magic_quotes_gpc") === "1") {
+			/* strip the slashes automatically inserted before doing more complete
+			 * escaping */
+			$input = stripslashes($input);
+		}
+
+		$output = mysql_real_escape_string($input);
+		return $output;
+	}
 }
 ?>
