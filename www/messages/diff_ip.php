a982276739f85b0bb7fa9480aa702089a4c74a48
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-08-10 14:22:38 +0200

    HTML-cleanup: Make different-IP notification pass W3C-XHTML validator Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- www/messages/diff_ip.php
+++ www/messages/diff_ip.php
@@ -1,19 +1,19 @@
 <?php
-echo "<HTML>\n";
-echo "   <HEAD><TITLE>Confusa Error Messaging Service</TITLE></HEAD>\n";
-echo "   <BODY>\n";
-echo "      <CENTER>\n";
-echo "         <H3>IP address notification</H3>\n";
+echo "<html>\n";
+echo "   <head><title>Confusa Error Messaging Service</title></head>\n";
+echo "   <body>\n";
+echo "      <center>\n";
+echo "         <h3>IP address notification</h3>\n";
 echo "         The address of the machine uploading a CSR is recorded.\n";
 echo "         If, for any reason, the address of your current machine differs from that, a notification";
-echo "         is displayed (it is colored in red). <BR />\n";
-echo "         <BR />\n";
-echo "         This may, or may not, mean anything, but you should be aware of this. <BR />\n";
-echo "         <BR /><BR />\n";
-echo "         Your current IP is " . $_SERVER['REMOTE_ADDR'] . "<BR />\n";
-echo "      </CENTER>\n";
-echo "   </BODY>\n";
-echo "</HTML>\n";
+echo "         is displayed (it is colored in red). <br />\n";
+echo "         <br />\n";
+echo "         This may, or may not, mean anything, but you should be aware of this. <br />\n";
+echo "         <br /><br />\n";
+echo "         Your current IP is " . $_SERVER['REMOTE_ADDR'] . "<br />\n";
+echo "      </center>\n";
+echo "   </body>\n";
+echo "</html>\n";
 
 ?>
 
