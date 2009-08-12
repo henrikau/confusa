b12a6874edd5cb24ff77aaa302484fa0c58f05c5
Henrik Austad <henrik@austad.us>
2009-08-12 12:54:02 +0200

    bash-libs: add bash-libs to share amongst the bash programs
    Add libraries for retrieving config and database connection from the
    bash-shell.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

---
+++ lib/bash/db_lib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Assumes that the config_lib has been loaded.
+
+username=`get_config_entry "mysql_username"`
+pw=`get_config_entry "mysql_password"`
+host=`get_config_entry "mysql_host"`
+db=`get_config_entry "mysql_db"`
+if [ "$pw" == "" ]; then
+    pw=""
+else
+    pw="-p$pw"
+fi
+MYSQL="/usr/bin/mysql -N -h$host -u$username -D$db $pw"
+function run_query ()
+{
+    if [ ! $# -eq 1 ]; then
+	echo "Need query" >&2
+	return -1
+    fi
+    $MYSQL -e"$1"
+}
