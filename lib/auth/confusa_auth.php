2234ba8f087d9a5db668de0a5eb3acb585ad321c
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-08-18 11:36:20 +0200

    Confusa_Auth: Rewrite to OO-design and new conventions
    Rewrite Confusa_Auth to an abstract base class with a handler class that
    can pick the right instance depending on the configuration.
    
    The class signature has been cleaned up to include only three hopefully
    self-explaining methods:
    	- authenticateUser()
    	- checkAuthentication()
    	- deAuthenticateUser()
    
    Different auth managers like IdP and OAuth can subclass the ABC and provide
    the specific functionality.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- lib/auth/confusa_auth.php
+++ lib/auth/confusa_auth.php
@@ -1,269 +1,96 @@
 <?php
-  /* Author: Henrik Austad <henrik.austad@uninett.no>
-   *
-   * Part of Confusa.
-   *
-   * This is the main authentication module of Confusa.
-   */
-/* get simplesaml */
-require_once 'config.php';
 
-if(!Config::get_config('auth_bypass'))
-{
-	/* Use the new autoloader functionality in SimpleSAMLphp */
-	$sspdir = Config::get_config('simplesaml_path');
-	require_once $sspdir . '/lib/_autoload.php';
-	SimpleSAML_Configuration::setConfigDir($sspdir . '/config');
-}
-require_once 'oauth_auth.php';
 require_once 'person.php';
-require_once 'logger.php';
-require_once 'debug.php';
-require_once 'mdb2_wrapper.php';
-/* global variable to check if the session has been started or not (avoid
- * multiple calls to simple_saml's session_start()
- */
-$session_started = false;
-/* global variable to determine if OAuth is used for authN
- */
-$use_oauth = false;
+require_once 'confusa_config.php';
+require_once 'config.php';
+$sspdir = Config::get_config('simplesaml_path');
+require_once $sspdir . '/lib/_autoload.php';
+SimpleSAML_Configuration::setConfigDir($sspdir . '/config');
 
-
-/* authenticate_user()
+/**
+ * Confusa_Auth - base class for all authentication managers
  *
- * This is the main function for checking if the user is authenticated.
+ * classes providing authN are supposed to implement all three of
+ * 		- authenticateUser()
+ * 		- checkAuthentication()
+ * 		- deAuthenticateUser()
  */
-function authenticate_user($person)
-    {
-    /* check to see if the person is authenticated. If the person is
-     * authenticated OK, is_authenticated will update the auth-fields of person,
-     * but also fill in all the remaining fields. 
-     */
-         $person = is_authenticated($person);
-
-    if (!$person->isAuth()) {
-        /* assert SSO
-         * Make sure the feide-login is OK.
-         */
-         _assert_sso($person);
-    }
-} /* end authenticate_user */
-
-function deauthenticate_user($person)
+abstract class Confusa_Auth
 {
-	if (isset($person)) {
-		$person->setAuth(false);
-	}
-}
+	/* the person that is authenticated by Confusa */
+	protected $person;
 
-/* is_authenticated()
- *
- * This function takes as argument a person, and checks if this person is authenticated.
- * *NOTE* this function does not check the fields of the person-object, it will
- * check the subsystem and *update* the values in person to reflect this.
- *
- * Hence; this is the authoriative-authentication source for any person.
- */
-function is_authenticated($person = null) {
-	if (!isset($person))
-		$person = new Person();
-
-	// Bypass auth
-	if(Config::get_config('auth_bypass'))
+	function __construct($person = NULL)
 	{
-		// Set some bogus attributes
-		$person->setName('Ola Nordmann');
-		$person->setEPPN('ola.nordmann@norge.no');
-		$person->setEmail('ola.nordmann@norge.no');
-		$person->setCountry('NO');
-		$person->setSubscriberOrgName('Test');
-		$person->setIdP('test-idp');
-		$person->setNREN('TEST-NREN');
-		$person->setEduPersonEntitlement('confusaAdmin');
-		$person->setAuth(true);
-		
-		return $person;
-	}
-		
-	/* check to see if the person is authN */
-	$config = _get_config();
-
-	global $use_oauth;
-	$use_oauth = (SimpleSAML_Module::isModuleEnabled('oauth') and isset($_REQUEST['oauth_consumer_key']));
-
-	if ($use_oauth) {
-		$oauth = ConfusaOAuth::getInstance();
-		$person->setAuth($oauth->isAuthorized());
-	} else {
-		$session = _get_session();
-		$person->setSession(_get_session());
-		$person->setSAMLConfiguration(SimpleSAML_Configuration::getInstance());
-		if (isset($session)) {
-			$person->setAuth($session->isValid());
+		if (is_null($person)) {
+			$this->person = new Person();
+		} else {
+			$this->person = $person;
 		}
 	}
 
-	if ($person->isAuth()) {
-		add_attributes($person);
+	function __destruct()
+	{
+		unset($this->person);
 	}
 
-	return $person;
-} /* end is_authenticated */
+	/**
+	 * Get the person object associated with this authN class
+	 *
+	 * @return the person member of this class
+	 */
+	public function getPerson()
+	{
+		return $this->person;
+	}
 
-
-/* add_attributes
- *
- * This function decorates a person-object, which must be non-null in order to
- * provide some sane abstraction away from the attributes-array.
- *
- * This in itself is not a good reason for using a dedicated object, but we try
- * to tie as many strings together (like attributes, authentication status etc)
- * and provide a sane interface to the outside world.
- */
-function add_attributes($person)
-{
-     $attributes = _get_attributes();
-
-     if (!isset($attributes['eduPersonPrincipalName'][0])) {
-	  $debug_string=__FILE__ .":".__LINE__." -> eduPersonPrincipalName not set!<BR>\n";
-	  Debug::dump($debug_string);
-          $person->setAuth(false);
-     }
-     else {
-	     $person->setName($attributes['cn'][0]);
-	     $person->setEPPN($attributes['eduPersonPrincipalName'][0]);
-	     $person->setEmail($attributes['mail'][0]);
-	     $person->setCountry($attributes['country'][0]);
-	     $person->setSubscriberOrgName($attributes['organization'][0]);
-	     $person->setEduPersonEntitlement($attributes['eduPersonEntitlement'][0]);
-	     $person->setNREN($attributes['nren'][0]);
-	     $person->setAuth(true);
-     }
-} /* end add_attributes() */
-
-/** logout_link
- *
- * params:
- * @logout_location:	If secondary logout is needed, or some logout-success
- *			message needs to be displayed, this is the page that the user will be
- *			redirected to.
- * @logout_name:	Content of the logout-link
- * @edu_name:		The unique feide name of the person we're logging out (so
- *			that the logout-form can remove info from the database).
- */
-function logout_link($logout_location="logout.php")
-{
-	if(Config::get_config('auth_bypass'))
-		return $logout_location;
-
-     $config = _get_config();
-
-     /* need to find the url, and handle some quirks in the result from selfURL
-      * in order to get proper url-base */
-     $base = SimpleSAML_Utilities::selfURL();
-     if (strpos($base, ".php"))
-          $base = dirname($base);
-     $link_base =  '/' . SimpleSAML_Configuration::getInstance()->getValue('baseurlpath') .
-			'saml2/sp/initSLO.php?RelayState='.$base .'/'. $logout_location;
-	return $link_base;
-} // end get_logout_link()
-
-
-function show_sso_debug($person) {
-	if(!Config::get_config('auth_bypass'))
-		return;
-		
-    if (!isset($person)) {
-        echo __FILE__ . ":" . __LINE__ . " person does not exist<BR>\n";
-        return;
-        
-        }
-    $config  = _get_config();
-    $session = _get_session();
-    if($person->isAuth()) {
-        $attributes = _get_attributes();
-        $time_left = $session->remainingTime();
-        $hours_left = (int)($time_left / 3600);
-        $mins_left = (int)(($time_left % 3600)/60);
-        $secs_left = (int)(($time_left % 3600) % 60);
-        printn("<HR>");
-        printBRn("<B>session and attributes from SimpleSAML debug info:</B>");
-        printBRn("Your session is valid for ". 
-                 $hours_left . "h " . 
-                 $mins_left  . "min " . 
-                 $secs_left  . "seconds<BR>");
-
-        printn("<table>");
-        foreach ($attributes AS $name => $value) {
-            /* several values in the field */
-            echo "\t<tr>\n\t\t<td>$name</td>\n\t\t<td>" . $value[0] . "</td>\n\t</tr>\n";
-        }
-        printn("</table>");
-        printn("<HR>");
-    }
-}
-/* gets the config from the SimpleSAML-package. (abstract layer, don't want this
- * in general code.
- *
- * Parameters: none
- * Returns: config-descriptor from simple-saml
- */
-
-function _get_config() 
-{
-    return SimpleSAML_Configuration::getInstance();
+	/**
+	 * Authenticate the idenitity of a user, using a free-of-choice method to be
+	 * implemented by subclasses
+	 */
+	public abstract function authenticateUser();
+	/**
+	 * Check (possibly by polling a subsystem), if a user is still authN.
+	 * @return True or false, reflecting the authN status
+	 */
+	public abstract function checkAuthentication();
+	/**
+	 * "Logout" the user, possibly using the subsystem. To be implemented by
+	 * subclasses
+	 */
+	public abstract function deAuthenticateUser();
 }
 
-/* _get_session()
+/**
+ * AuthHandler - return the right authentication manager for the configuration
  *
- * Returns: the session-descriptor from SimpelSAML. If the session has not been
- * started, this function will also start a new session for you. :-)
+ * The handler should abstract that decision away from the calling functions
+ * and consult on its own on the configuration or environment
  */
-function _get_session() 
+require_once 'idp.php';
+require_once 'bypass.php';
+class AuthHandler
 {
-    // ensure that session_start() is only called once
-    global $session_started;
-    if (!$session_started) {
-        session_start();
-        $session_started=true;
-    }
-    return SimpleSAML_Session::getInstance();
-}
+	private static $auth;
+	/**
+	 * Get the auth manager based on the request
+	 *
+	 * @param $person The person for which the auth_manager should be created
+	 * @return an instance of Confusa_Auth
+	 */
+	public static function getAuthManager($person)
+	{
+		if (!isset(AuthHandler::$auth)) {
 
-function _get_attributes()
-{ 
-	global $use_oauth;
-	if ($use_oauth) {
-		return ConfusaOAuth::getInstance()->getAttributes();
-	} else {
-		return _get_session()->getAttributes();
+			if (Config::get_config('auth_bypass') === TRUE) {
+				AuthHandler::$auth = new Confusa_Auth_Bypass($person);
+			} else {
+				AuthHandler::$auth = new Confusa_Auth_IdP($person);
+			}
+		}
+
+		return AuthHandler::$auth;
 	}
 }
-
-
-function _assert_sso($person)
-{
-  $config  = _get_config();
-  $session = _get_session();
-
-  /* Check if valid local session exists..
-   *
-   * If not:
-   *	session set
-   *	session valid
-   * Do:
-   *	set new header
-   * http://rnd.feide.no/content/using-simplesamlphp-service-provider#id436365
-   */
-  if (!isset($session) || !$session->isValid() ) {
-       $relay = Config::get_config('server_url') . Config::get_config('post_login_page');
-       SimpleSAML_Utilities::redirect('/' . $config->getBaseURL() . 'saml2/sp/initSSO.php',array('RelayState' => $relay));
-       exit(0);
-  }
-
-  /* update person, FIXME: update attributes as well */
-  $person->setAuth($session->isValid());
-  add_attributes($person);
-} /* end  _assert_sso() */
 
 ?>
