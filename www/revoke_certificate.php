cc6494e220b51e73508257240f951c07fe2023c3
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-08-19 13:59:15 +0200

    Revoke certificate: Replace exceptions by error messages
    A www/ class should not throw any exceptions
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- www/revoke_certificate.php
+++ www/revoke_certificate.php
@@ -109,12 +109,12 @@ class RevokeCertificate extends FW_Content_Page
 	{
 		if (!$this->person->isAdmin()) {
 			Logger::log_event(LOG_ALERT, "User " . $this->person->getX509ValidCN() . " allowed to set admin-mode, but is not admin");
-			throw new ConfusaGenException("Impossible condition. NON-Admin user in admin-mode!");
+			Framework::error_output("Impossible condition. NON-Admin user in admin-mode!");
 		}
 
 		/* Test access-rights */
 		if (!$this->person->isSubscriberAdmin() && !$this->person->isSubscriberSubAdmin())
-			throw new ConfusaGenException("Insufficient rights for revocation!");
+			Framework::error_output("Insufficient rights for revocation!");
 
 		$this->tpl->assign('file_name', 'eppn_list');
 
@@ -193,8 +193,6 @@ class RevokeCertificate extends FW_Content_Page
 	 *                       revoked
 	 * @param $reason The reason for revocation as defined in RFC 3280
 	 *
-	 * @throws ConfusaGenException If revocation fails or the revocation reason is
-	 *                             unrecognized
 	 */
 	private function revoke_certs($auth_key_list, $reason)
 	{
@@ -202,7 +200,7 @@ class RevokeCertificate extends FW_Content_Page
 		$auth_key_list = $this->sanitize($auth_key_list);
 
 		if (array_search($reason, $this->nren_reasons) === FALSE) {
-			throw new ConfusaGenException("Encountered an unknown revocation " .
+			Framework::error_output("Encountered an unknown revocation " .
 										  "reason!"
 			);
 		}
@@ -231,9 +229,6 @@ class RevokeCertificate extends FW_Content_Page
 	 *
 	 * @param string $reason The reason for revocation (as in RFC 3280)
 	 *
-	 * @throws ConfusaGenException If the auth_keys are not found in the session,
-	 *                             there is a problem with revocation or if the
-	 *                             reason is unknown
 	 */
 	private function revoke_list($reason)
 	{
@@ -243,12 +238,12 @@ class RevokeCertificate extends FW_Content_Page
 			$auth_keys = $_SESSION['auth_keys'];
 			unset($_SESSION['auth_keys']);
 		} else {
-			throw new ConfusaGenException("Lost session! Please log-out of Confusa, " .
-										  "log-in again and try again!\n");
+			Framework::error_output("Lost session! Please log-out of Confusa, " .
+									"log-in again and try again!\n");
 		}
 
 		if (array_search($reason, $this->nren_reasons) === false) {
-			throw new ConfusaGenException("Unknown reason for certificate revocation!");
+			Framework::error_output("Unknown reason for certificate revocation!");
 		}
 
 		$num_certs = count($auth_keys);
@@ -279,7 +274,6 @@ class RevokeCertificate extends FW_Content_Page
 	 *                          CSV of eduPersonPrincipalNames
 	 * @param Person $person The person calling this function
 	 *
-	 * @throws FileException if something goes wrong in parsing the CSV file
 	 */
 	private function search_list_display($eppn_file)
 	{
