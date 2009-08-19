fcf77bab63be2e50769e7e551bc34ccff25edb41
Henrik Austad <henrik@austad.us>
2009-08-19 09:20:58 +0200

    ConfusaAttributeMap : parse organization from eduPersonOrgDN
    We ask the NRENs/IdPs to export the attribute eduPersnoOrgDN which has
    the expected form
    
    	o=Hogwarts,dc=hsww,dc=wiz
    
    We want the first element, i.e. 'Hogwarts'.
    
    On top of this, it should be added in all lower-case, and without
    illegal characters.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- include/ConfusaAttributeMap.php
+++ include/ConfusaAttributeMap.php
@@ -30,6 +30,7 @@ class sspmod_core_Auth_Process_ConfusaAttributeMap extends SimpleSAML_Auth_Proce
                }
 	     }
 	     $this->fix_entitlement($request);
+	     $this->fixSubscriberName($request);
      }
 
      private function fix_surfnet(&$request) {
@@ -43,7 +44,6 @@ class sspmod_core_Auth_Process_ConfusaAttributeMap extends SimpleSAML_Auth_Proce
           if (isset($request['Attributes']['urn:mace:dir:attribute-def:mail'][0]))
                $request['Attributes']['mail'] = $request['Attributes']['urn:mace:dir:attribute-def:mail'];
 
-          $request['Attributes']['organization'][0] = "surfnet";
      }
 
      private function fix_haka(&$request) {
@@ -54,7 +54,6 @@ class sspmod_core_Auth_Process_ConfusaAttributeMap extends SimpleSAML_Auth_Proce
           if (isset($request['Attributes']['urn:oid:0.9.2342.19200300.100.1.3'][0]))
                $request['Attributes']['mail'] = $request['Attributes']['urn:oid:0.9.2342.19200300.100.1.3'];
 
-          $request['Attributes']['organization'][0] = "Haka";
      }
 
      private function fix_wayf(&$request) {
@@ -65,7 +64,6 @@ class sspmod_core_Auth_Process_ConfusaAttributeMap extends SimpleSAML_Auth_Proce
           if (isset($request['Attributes']['mail'][0]))
                $request['Attributes']['mail'][0] = array(base64_decode($request['Attributes']['mail'][0]));
 
-          $request['Attributes']['organization'][0] = "WAYF";
      }
 
      private function fix_entitlement(&$request)
@@ -74,5 +72,43 @@ class sspmod_core_Auth_Process_ConfusaAttributeMap extends SimpleSAML_Auth_Proce
 		     $request['Attributes']['eduPersonEntitlement'][0] = "confusaAdmin";
 	     }
      }
+
+     /**
+      * fixSubscriberName() - parse the ePODN to find the subscriber name
+      *
+      * We expect the IdP to export the eduPersonOrgDN (ePODN) as described in
+      *
+      *			http://rnd.feide.no/attribute/edupersonorgdn
+      *
+      * and the expected form is:
+      *
+      *			o=Hogwarts, dc=hsww, dc=wiz
+      *
+      * We want the 'highest' attribute, i.e. the attribute first in the chain
+      * as this normally describes the institution.
+      */
+     private function fixSubscriberName(&$request)
+     {
+	     /* Is ePODN set? */
+	     if (!isset($request['Attributes']['eduPersonOrgDN'][0]) || $request['Attributes']['eduPersonOrgDN'][0] == "") {
+		     echo "<h3>Error!</h3>\n";
+		     echo "Your IdP does not export all required attributes. The attribute ";
+		     echo "<a href=\"http://rnd.feide.no/attribute/edupersonorgdn\">eduPersonOrgDN</a> is not set!<br /><br />\n";
+		     echo "Please notify your local IT-support about this problem.\n";
+		     exit(1);
+	     }
+	     $org = explode(",", $request['Attributes']['eduPersonOrgDN'][0]);
+	     if (is_array($org)) {
+		     $org = explode("=", $org[0]);
+		     if (is_array($org)) {
+			     $request['Attributes']['organization'][0] = $org[1];
+		     } else {
+			     $request['Attributes']['organization'][0] = $org;
+		     }
+	     } else {
+		     $request['Attributes']['organization'][0] = "__error__";
+	     }
+	     $request['Attributes']['organization'][0] = strtolower($request['Attributes']['organization'][0]);
+     } /* end fixSusbscriberName() */
 }
 ?>
