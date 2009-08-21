4ac41d770b26fd5260dcf4b30aae065062a90ea4
Henrik Austad <henrik@austad.us>
2009-08-21 16:25:18 +0200

    csr_lib: fix typo in function-name to error_output
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- lib/key/csr_lib.php
+++ lib/key/csr_lib.php
@@ -45,7 +45,7 @@ function test_content($content, $auth_url)
    */
   $length = Config::get_config('key_length');
   if (csr_pubkey_length($content) < $length) {
-	  Framework::error_putput("Uploaded key is not long enough. Please download a proper keyscript and try again.");
+	  Framework::error_output("Uploaded key is not long enough. Please download a proper keyscript and try again.");
        return false;
   }
 
