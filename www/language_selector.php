650632fbf2926c5010314db9037682b2a897e163
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-09-01 16:14:45 +0200

    Language_selector: Notify user upon successful operations
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- www/language_selector.php
+++ www/language_selector.php
@@ -9,9 +9,13 @@ require_once 'translator.php';
 final class CP_Language_Selector extends FW_Content_Page
 {
 
+	private $full_names;
+
 	function __construct()
 	{
 		parent::__construct("Language Selector", true);
+		$available_languages = Config::get_config('language.available');
+		$this->full_names = Translator::getFullNamesForISOCodes($available_languages);
 	}
 
 	public function pre_process($person)
@@ -59,11 +63,9 @@ final class CP_Language_Selector extends FW_Content_Page
 			$current_language = Config::get_config('language.default');
 		}
 
-		$available_languages = Config::get_config('language.available');
-		$full_names = Translator::getFullNamesForISOCodes($available_languages);
-		$this->tpl->assign('languages', $full_names);
+		$this->tpl->assign('languages', $this->full_names);
 		$this->tpl->assign('current_language', $current_language);
-		$this->tpl->assign('language_codes', $available_languages);
+		$this->tpl->assign('language_codes', array_keys($this->full_names));
 		$this->tpl->assign('content', $this->tpl->fetch('language_selector.tpl'));
 	}
 
@@ -81,13 +83,19 @@ final class CP_Language_Selector extends FW_Content_Page
 			Framework::error_output("Updating the language to $new_language for your " .
 									"NREN $nren failed, probably due to problems with " .
 									"the supplied data. Server said: " . $dbqe->getMessage());
+			return;
 		} catch (DBStatementException $dbse) {
 			Logger::log_event(LOG_NOTICE, "Updating the language to $new_language " .
 							"failed for NREN $nren. Error was: " . $dbse->getMessage());
 			Framework::error_output("Updating the language to $new_language for your " .
 									"NREN $nren failed, probably due to problems with the " .
 									"server configuration! Server said: " . $dbse->getMessage());
+			return;
 		}
+
+		Logger::log_event(LOG_DEBUG, "Default language changed to $new_language for NREN $nren");
+		Framework::success_output("Default language for your NREN successfully changed to " .
+									$this->full_names[$new_language]);
 	}
 
 	private function updateSubscriberLanguage($subscriber, $new_language)
@@ -104,13 +112,20 @@ final class CP_Language_Selector extends FW_Content_Page
 			Framework::error_output("Updating the language to $new_language failed " .
 									"for subscriber $subscriber, probably due to problems " .
 									"with the supplied data. Server said: " . $dbqe->getMessage());
+			return;
 		} catch (DBStatementException $dbse) {
 			Logger::log_event(LOG_NOTICE, "Updating the language to $new_language " .
 							"faield for subscriber $subscriber. Error was: " . $dbse->getMessage());
 			Framework::error_output("Updating the language to $new_language failed " .
 									"for subscriber $subscriber, probably due to problems " .
 									"with the server configuration. Server said: " . $dbse->getMessage());
+			return;
 		}
+
+		Logger::log_event(LOG_DEBUG, "Default language changed to $new_language for " .
+							"subscriber $subscriber");
+		Framework::success_output("Default language for your subscriber successfully changed to " .
+									$this->full_names[$new_language]);
 	}
 
 	private function getNRENLanguage($nren)
