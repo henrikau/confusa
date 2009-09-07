9585ff44cde24a8444504dc349c6e70ad3517272
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-09-07 17:57:52 +0200

    Cert-request JavaScript: Add IE-Vista request generation/install methods
    In Vista, the certificate generation method was changed from XEnroll to
    CertEnroll, now requiring fairly complex ActiveX objects. The Confusa
    instance will have to be added to the trusted sites of the user, otherwise
    it will not work. ActiveX will have to be enabled on the client's machine.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- www/js/cert_request.js
+++ www/js/cert_request.js
@@ -14,6 +14,13 @@ function checkCRMF() {
     }
 }
 
+function checkWindowsRequest(request) {
+    with (document.reqForm) {
+	browserRequest.value = request;
+	submit();
+    }
+}
+
 /**
  * This is mostly about visual feedback while the certificate is still
  * processed
@@ -49,6 +56,53 @@ function statusDone(key) {
     document.getElementById("reqForm").style.display = "none";
 }
 
+function createIEVistaRequest(dn, keysize)
+{
+    var classFactory = new ActiveXObject("X509Enrollment.CX509EnrollmentWebClassFactory");
+
+    // Declaration of the objects
+    var objEnroll = classFactory.CreateObject("X509Enrollment.CX509Enrollment");
+    var objPrivateKey = classFactory.CreateObject("X509Enrollment.CX509PrivateKey");
+    var objRequest = classFactory.CreateObject("X509Enrollment.CX509CertificateRequestPkcs10");
+    var objDN = classFactory.CreateObject("X509Enrollment.CX500DistinguishedName");
+
+    // Specify the name of the cryptographic provider.
+    objPrivateKey.ProviderName = "Microsoft Enhanced RSA and AES Cryptographic Provider";
+
+    /* allow signing */
+    objPrivateKey.KeySpec = 0x2;
+    objPrivateKey.Length=keysize;
+    /* use "RSA-full" as the key algorithm */
+    objPrivateKey.ProviderType = "1";
+    objRequest.InitializeFromPrivateKey(1, objPrivateKey, "");
+    objDN.Encode(dn, 0);
+    objRequest.Subject = objDN;
+    objEnroll.InitializeFromRequest(objRequest);
+    request = objEnroll.CreateRequest(1);
+    checkWindowsRequest(request);
+    return false;
+}
+
+function createMozillaRequest(dn, keysize)
+{
+    if (!confirm("Really request and sign a new X.509 certificate for\nDN " + dn + "?")) {
+	return false;
+    }
+
+    try {
+	crmf=crypto.generateCRMFRequest(dn, "regToken", "authenticator", null, "checkCRMF();" , keysize, null, "rsa-dual-use");
+    } catch (e) {
+	alert("Could not generate a new certificate signing request.\nProblem is " + e);
+    }
+
+    if (crmf.request.substring(0,6) == "error:") {
+	    alert("Error occured: " + crmf);
+	    return false;
+    }
+
+    return false;
+}
+
 /**
  * Create a request from the respective DN
  * Firefox currently only supports doing that with the CRMF and Netscape SPKAC
@@ -56,30 +110,39 @@ function statusDone(key) {
  */
 function createRequest(dn, keysize)
 {
-
+	/* Firefox, Mozilla */
 	if (window.crypto) {
+		return createMozillaRequest(dn, keysize);
 
-		if (!confirm("Really request and sign a new X.509 certificate for\nDN " + dn + "?")) {
-		    return false;
+	} else if (navigator.userAgent.indexOf("MSIE") > -1) {	/* Internet explorer */
+		if (navigator.userAgent.indexOf("Windows NT 5.1") == -1) { /* Windows Vista and later */
+			return createIEVistaRequest(dn, keysize);
 		}
-
-		try {
-			crmf=crypto.generateCRMFRequest(dn, "regToken", "authenticator", null, "checkCRMF();" , keysize, null, "rsa-dual-use");
-		} catch (e) {
-		    alert("Could not generate a new certificate signing request.\nProblem is " + e);
-		}
-
-		if (crmf.request.substring(0,6) == "error:") {
-			alert("Error occured: " + crmf);
-			return false;
-		}
-
-		return false;
-
 	} else {
 		alert("Your browser is currently not supported.\nSupported browsers: Firefox/Mozilla");
 		return false;
 	}
+}
+
+function installIEVistaCertificate()
+{
+    var classFactory = new ActiveXObject("X509Enrollment.CX509EnrollmentWebClassFactory");
+    var objEnroll = classFactory.CreateObject("X509Enrollment.CX509Enrollment");
+    /*
+     * 0x4 in the first parameter: allow untrusted root
+     * 0x1 in the last paremeter: certificate is base-64 encoded
+     */
+    objEnroll.installResponse(0x4, g_ccc, 0x1);
+}
+
+function installMozillaCertificate()
+{
+    try {
+	var error = window.crypto.importUserCertificates("Confusa certificate", g_ccc, true);
+    } catch (e) {
+	    alert("Installation FAILED!\nNote that you can only install " +
+	    "certificates for browser-generated requests! ");
+    }
 }
 
 /**
@@ -89,10 +152,17 @@ function createRequest(dn, keysize)
  */
 function installCertificate()
 {
-	try {
-		var error = window.crypto.importUserCertificates("Confusa certificate", g_ccc, true);
-	} catch (e) {
-		alert("Installation FAILED!\nNote that you can only install " +
-		"certificates for browser-generated requests! ");
+
+	/* Firefox, Mozilla */
+	if (window.crypto) {
+	    installMozillaCertificate();
+	} else if (navigator.userAgent.indexOf("MSIE") > -1) {	/* Internet explorer */
+		if (navigator.userAgent.indexOf("Windows NT 5.1") == -1) { /* Windows Vista and later */
+			installIEVistaCertificate();
+		}
+	} else {
+		alert("Your browser is currently not supported.\nSupported browsers: Firefox/Mozilla");
+		return false;
 	}
+
 }
