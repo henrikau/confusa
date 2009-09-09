563e9dbf8023370509dd708bf1407a5c279a8730
Henrik Austad <henrik@austad.us>
2009-09-09 08:40:37 +0200

    robot.php move pre_process logic to pre_process and create 'proper' constructor
    The constructor was wrongly used as pre_process, and thus it failed when
    accessing $person. Moreover, the page was never marked as 'sensitive',
    something it most certainly is.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- www/robot.php
+++ www/robot.php
@@ -3,17 +3,25 @@ require_once 'confusa_include.php';
 require_once 'framework.php';
 require_once 'person.php';
 require_once 'mdb2_wrapper.php';
+require_once 'cert_lib.php';
 
 class CP_Robot_Interface extends Content_Page
 {
 	function __construct()
 	{
-		parent::__construct("Robot", true);
+		parent::__construct("Robot Interface", true);
+		Framework::sensitive_action();
+	}
+	function pre_process($person)
+	{
+		parent::pre_process($person);
 		if (isset($_POST['robot_action'])) {
 			$action = Input::sanitize($_POST['robot_action']);
 			switch($action) {
 			case 'paste_new':
-				$this->handlePasteCertificate(Input::sanitize($_POST['cert']));
+				if (isset($_POST['cert']) && $_POST['cert'] != "") {
+					$this->insertCertificate($_POST['cert']);
+				}
 				break;
 			case 'upload_new':
 				$this->handleFileCertificate();
