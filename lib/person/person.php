5335d8ec756c6f7341293793fa4c7fa13ec4db4c
Henrik Austad <henrik@austad.us>
2009-10-06 08:27:57 +0200

    person: remove the initial test for admins-columns
    As the default value is an empty string, PHP will treat the array-index
    as not set, which will bomb on every new admin.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- lib/person/person.php
+++ lib/person/person.php
@@ -774,15 +774,6 @@ class Person{
 	    db_array_debug($res);
 	    if ($size == 1) {
 		    $adminRes = $res[0]['admin_level'];
-		    /* Is the name and other variables properly set? */
-		    if (!isset($res[0]['admin_name']) || !isset($res[0]['admin_email'])) {
-			    Framework::error_output("[$errorCode] database is inconsistent. Please contact operational support.");
-			    $msg = "[$errorCode] Missing elements in the admin-table. Please update the table to current standard.";
-			    Logger::log_event(LOG_ALERT, $msg);
-			    $this->adminDBError = true;
-			    return NORMAL_USER;
-		    }
-
 		    if ($this->getX509ValidCN() != $res[0]['admin_name'] ||
 			$this->getEmail() != $res[0]['admin_email']) {
 			    try {
