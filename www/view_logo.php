eb7d9701386f687fc1879bf3c4ddb70f32388566
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-10-06 10:47:09 +0200

    View-logo: Display the default 'sigma'-logo, if no custom logo found
    Restores the old behaviour.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- www/view_logo.php
+++ www/view_logo.php
@@ -9,32 +9,37 @@ require_once 'confusa_constants.php';
  */
 
 $nren = Input::sanitize($_GET['nren']);
-$logo_path = Config::get_config('custom_logo') . $nren . '/custom.';
 
-/*
- * Search if there is one custom.png, custom.jpg or custom.any_other_
- * allowed_suffix file in the custom-logo folder.
- *
- * If there isn't return null
- */
-foreach(ConfusaConstants::$ALLOWED_IMG_SUFFIXES as $suffix) {
-	if (file_exists($logo_path . $suffix)) {
-		$logo_suffix = $suffix;
-		break;
+if (isset($nren)) {
+	$logo_path = Config::get_config('custom_logo') . $nren . '/custom.';
+
+	/*
+	 * Search if there is one custom.png, custom.jpg or custom.any_other_
+	 * allowed_suffix file in the custom-logo folder.
+	 *
+	 * If there isn't return null
+	 */
+	foreach(ConfusaConstants::$ALLOWED_IMG_SUFFIXES as $suffix) {
+		if (file_exists($logo_path . $suffix)) {
+			$logo_suffix = $suffix;
+			break;
+		}
 	}
 }
 
 if (empty($logo_suffix)) {
-	echo "";
+	$logo_name = Config::get_config('install_path') . '/www/graphics/logo-sigma.png';
+	$logo_suffix = "png";
 } else {
 	$logo_name = $logo_path . $logo_suffix;
-	$fp = fopen($logo_name, "r");
-	$image = fread($fp, filesize($logo_name));
-	fclose($fp);
-
-	header("Content-type: image/$logo_suffix");
-	echo $image;
 }
+
+$fp = fopen($logo_name, "r");
+$image = fread($fp, filesize($logo_name));
+fclose($fp);
+
+header("Content-type: image/$logo_suffix");
+echo $image;
 
 
 ?>
