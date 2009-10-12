b94cc215b3f2e17f84ea46f8d3a9df516b9a9147
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-10-12 19:27:26 +0200

    Views-create: Add subscriber-DN to nren_subscriber_view
    Since that is polled from the revocation class and the revocation class
    needs the DN for the subscriber, not the IdP-name.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- init/views_create.sql
+++ init/views_create.sql
@@ -8,12 +8,14 @@ CREATE OR REPLACE ALGORITHM = TEMPTABLE
 VIEW nren_subscriber_view
 	(subscriber_id,
 	subscriber,
+	subscriber_dn,
 	org_state,
 	nren,
 	nren_id)
 AS SELECT
 	s.subscriber_id,
 	s.name,
+	s.dn_name,
 	s.org_state,
 	n.name,
 	n.nren_id
