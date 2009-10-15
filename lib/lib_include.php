0b24f70c4c28c64767af1c266590bc8613ed7ea5
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-10-16 00:22:45 +0200

    Lib-include: Include /etc/confusa/ if it contains the config-file
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- lib/lib_include.php
+++ lib/lib_include.php
@@ -11,6 +11,11 @@ $path .= PATH_SEPARATOR . LIB_DIR . '/actors';
 $path .= PATH_SEPARATOR . LIB_DIR . '/framework';
 $path .= PATH_SEPARATOR . LIB_DIR . '/input';
 $path .= PATH_SEPARATOR . LIB_DIR . '/robot';
+
+if (file_exists('/etc/confusa/confusa_config.php') === true) {
+    $path .= PATH_SEPARATOR . '/etc/confusa';
+}
+
 ini_set('include_path', $path);
 
 
