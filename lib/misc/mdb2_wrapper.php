13ab368940eaad92b476059b979e0a134f25f1ec
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-10-21 18:40:13 +0200

    MDB2Wrapper: Called $res->getMessage() outside of quotes
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- lib/misc/mdb2_wrapper.php
+++ lib/misc/mdb2_wrapper.php
@@ -56,7 +56,7 @@ class MDB2Wrapper
 	  $res = $stmnt->execute($data);
           if (PEAR::isError($res)) {
 		  $errorCode = create_pw(8);
-		  $logMsg  = "[$errorCode] Query failed: $res->getMessage() - "  . $res->getUserInfo();
+		  $logMsg  = "[$errorCode] Query failed: " . $res->getMessage() . " - ". $res->getUserInfo();
 		  if (Config::get_config('debug')) {
 			  $logMsg .= "[Debug]: " . $res->getDebugInfo();
 		  }
