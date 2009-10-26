03ab78901ef7a268691757052f4e2f51df511c4d
Henrik Austad <henrik@austad.us>
2009-10-26 09:34:57 +0100

    robot_www: Verify all environment variables before using them.
    Before we use an environment-variable, we must check to see if is
    there. This will not affect the tests any way, but warnings  will be
    removed from output.
    
    This resolves #123:
    http://www.assembla.com/spaces/confusa/tickets/123-verify-all-env--variables-in-confusa_robot
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- robot_www/index.php
+++ robot_www/index.php
@@ -21,18 +21,44 @@ $log_error_code = create_pw(8);
 function assertEnvironment()
 {
 	global $log_error_code;
-	/* are we on SSL */
-	if (strtolower($_SERVER['HTTPS']) != 'on') {
-		Logger::log_event(LOG_NOTICE, "[RI] ($log_error_code) Server is not running on SSL. Blocking robot-connections.");
+
+	/*
+	 * are we on SSL
+	 */
+	if (is_null($_SERVER['HTTPS'])) {
+		Logger::log_event(LOG_NOTICE,
+				  "[RI] ($log_error_code) Environment-variable 'HTTP' not available.");
 		exit(0);
 	}
-	/* SSLv3 */
+	if (strtolower($_SERVER['HTTPS']) != 'on') {
+		Logger::log_event(LOG_NOTICE,
+				  "[RI] ($log_error_code) Server is not running on SSL. Blocking robot-connections.");
+		exit(0);
+	}
+	/*
+	 * SSLv3
+	 */
+	if (is_null($_SERVER['SSL_PROTOCOL'])) {
+		Logger::log_event(LOG_NOTICE,
+				  "[RI] ($log_error_code) Environment-variable 'SSL_PROTOCL' not available.");
+		exit(0);
+	}
 	$protocol = strtolower($_SERVER['SSL_PROTOCOL']);
 	if (!($protocol == 'sslv3' || $protocol == 'tlsv1')) {
-		Logger::log_event(LOG_NOTICE, "[RI] ($log_error_code) Not on proper ssl protocol. Need SSLv3/TLS. Got " . $_SERVER['SSL_PROTOCOL']);
+		Logger::log_event(LOG_NOTICE,
+				  "[RI] ($log_error_code) Not on proper ssl protocol. Need SSLv3/TLS. Got " .
+				  $_SERVER['SSL_PROTOCOL']);
 		exit(0);
 	}
-	/* do we have a client certificate? */
+
+	/*
+	 * do we have a client certificate?
+	 */
+	if (is_null($_SERVER['SSL_CLIENT_CERT'])) {
+		Logger::log_event(LOG_NOTICE,
+				  "[RI] ($log_error_code) Environment-variable 'SSL_CLIENT_CERT' not available.");
+		exit(0);
+	}
 	$cert = $_SERVER['SSL_CLIENT_CERT'];
 	if (!isset($cert) || $cert == "") {
 		Logger::log_event(LOG_NOTICE, "[RI] ($log_error_code) Connection from client (".
@@ -40,6 +66,15 @@ function assertEnvironment()
 				  ") without certificate. Dropping connection.");
 		exit(0);
 	}
+
+	/*
+	 * Is the certificate properly constructed (can Apache find the DN)?
+	 */
+	if (is_null($_SERVER['SSL_CLIENT_I_DN'])) {
+		Logger::log_event(LOG_NOTICE, "Malformed certificate from " . $_SERVER['REMOTE_ADDR'] . ". Aborting.");
+		exit(0);
+	}
+
 	return true;
 } /* end assertEnvironment() */
 
