2f68d57b183c21f0a10c435bf1acce026d750919
Henrik Austad <henrik@austad.us>
2009-11-13 16:17:57 +0100

    CountryMap: update documentation of class (comments)
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- include/CountryMap.php
+++ include/CountryMap.php
@@ -1,20 +1,36 @@
 <?php
-/* Henrik Austad, 2009
- *
- * Part of Confusa, GPLv3 applies.
- *
- * Simple class for finding the country the user comes from.
- * By mapping the idp to country, we have a very easy and secure way of
- * determining the country the user comes from.
- *
- * One problem might be OpenID, but for all the national federations, this
- * should be accurate enough.
- */
+  /**
+   * CountryMap - map the IdP to a country.
+   *
+   * This map will use the IdP-identifier to retrieve the country. As an IdP can
+   * only belong to a single NREN, and each NREN can only belong to a single
+   * country, we can match the IdP to the country exactly.
+   *
+   * This file must be added to simplesamlphp/config/config.php::authproc.sp
+   * (see INSTALL for instructions) and symlinked to
+   *	<simplesamlphp>/modules/core/lib/Auth/Process/
+   *
+   * @author	Henrik Austad <henrik.austad@uninett.no>
+   */
 class sspmod_core_Auth_Process_CountryMap extends SimpleSAML_Auth_ProcessingFilter {
 	private $known_idps = array();
 
 	public function __construct($config, $reserved) {
 		parent::__construct($config, $reserved);
+
+          /* set the known idps.
+	   * These names will be matched against $request['Source'][entityid'],
+	   * which is the array-key in simpelsamlphp's saml20-idp-remote.php
+	   */
+          $this->known_idps = array('https://openidp.feide.no'			=> 'NO',
+				    'https://idp-test.feide.no'			=> 'NO',
+				    'https://idp.feide.no'			=> 'NO',
+                                    'edugain.showcase.surfnet.nl'		=> 'NL',
+                                    'https://testidp.wayf.dk'			=> 'DK',
+                                    'https://betawayf.wayf.dk'			=> 'DK',
+                                    'https://aitta2.funet.fi/idp/shibboleth'	=> 'FI'
+               );
+     }
 
 		/* set the known idps.
 		 * These names will be matched against $request['Source'][entityid'],
