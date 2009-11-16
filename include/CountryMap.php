c8b1380972a85d62c7081d9e2ffbd64fc336f7c3
Henrik Austad <henrik@austad.us>
2009-11-16 11:36:44 +0100

    Map: add fedex.terena.org idp to list of known IdP's
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- include/CountryMap.php
+++ include/CountryMap.php
@@ -11,24 +11,35 @@
  * should be accurate enough.
  */
 class sspmod_core_Auth_Process_CountryMap extends SimpleSAML_Auth_ProcessingFilter {
-     private $known_idps = array();
+	private $known_idps = array();
 
-     public function __construct($config, $reserved) {
-          parent::__construct($config, $reserved);
-          /* set the known idps */
-          $this->known_idps = array('https://openidp.feide.no'			=> 'NO',
-				    'https://idp-test.feide.no'			=> 'NO',
-				    'https://idp.feide.no'			=> 'NO',
-                                    'edugain.showcase.surfnet.nl'		=> 'NL',
-                                    'https://testidp.wayf.dk'			=> 'DK',
-                                    'https://betawayf.wayf.dk'			=> 'DK',
-                                    'https://aitta2.funet.fi/idp/shibboleth'	=> 'FI'
-               );
-     }
+	public function __construct($config, $reserved) {
+		parent::__construct($config, $reserved);
 
-     public function process(&$request) {
-          $idp = $request['Source']['entityid'];
-          $request['Attributes']['country'] = array($this->known_idps[$idp]);
-     }
-}
+		/* set the known idps.
+		 * These names will be matched against $request['Source'][entityid'],
+		 * which is the array-key in simpelsamlphp's saml20-idp-remote.php
+		 */
+		$this->known_idps = array('https://openidp.feide.no'			=> 'NO',
+					  'https://idp-test.feide.no'			=> 'NO',
+					  'https://idp.feide.no'			=> 'NO',
+
+					  'edugain.showcase.surfnet.nl'		=> 'NL',
+					  'https://fedex.terena.org'			=> 'NL',
+
+					  'https://testidp.wayf.dk'			=> 'DK',
+					  'https://betawayf.wayf.dk'			=> 'DK',
+
+					  'https://aitta2.funet.fi/idp/shibboleth'	=> 'FI'
+			);
+	}
+
+	public function process(&$request) {
+		$idp = $request['Source']['entityid'];
+		if (is_null($request['Attributes']['idp'])) {
+			$request['Attributes']['idp'] = array($idp);
+		}
+		$request['Attributes']['country'] = array($this->known_idps[$idp]);
+	}
+  }
 ?>
