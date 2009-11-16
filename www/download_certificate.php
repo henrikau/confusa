73e1706cd603287c442ee990f13ac059db64e188
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-11-16 19:32:50 +0100

    Download_certificate: Return false in mail_cert if certificate not set
    Thus, not both messages ("Certificate is being processed" and "Your email
    has been sent") show up.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- www/download_certificate.php
+++ www/download_certificate.php
@@ -161,9 +161,12 @@ final class CP_DownloadCertificate extends Content_Page
 					Framework::error_output("Could not send mail properly!");
 					return false;
 				}
+			} else {
+				return false;
 			}
 		} catch (ConfusaGenException $e) {
 			Framework::error_output("Could not mail the certificate, server said: " . htmlentities($e->getMessage()));
+			return false;
 		}
 		Framework::success_output("Your e-mail has been sent");
 	} /* end send_cert */
