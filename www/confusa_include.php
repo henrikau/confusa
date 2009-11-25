81d0dc05c3b0c34573114da66ce011f972d59717
Henrik Austad <henrik@austad.us>
2009-11-25 13:22:50 +0100

    confusa_include: fix path for mailer
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- www/confusa_include.php
+++ www/confusa_include.php
@@ -18,7 +18,9 @@ define("CA_STANDALONE", 0);
 define("CA_ONLINE", 1);
 
 define('WEB_DIR', dirname($_SERVER['SCRIPT_FILENAME']));
+$path .= PATH_SEPARATOR . "/usr/share/php/libphp-phpmailer/";
 $path .= PATH_SEPARATOR . dirname(WEB_DIR) . '/';
+$path .= PATH_SEPARATOR . dirname(WEB_DIR) . '/www';
 $path .= PATH_SEPARATOR . dirname(WEB_DIR) . '/lib';
 $path .= PATH_SEPARATOR . dirname(WEB_DIR) . '/programs';
 $path .= PATH_SEPARATOR . dirname(WEB_DIR) . '/config';
