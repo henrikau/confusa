ba2f8ad00d2d8a35c4037ee4966c491ec36896e8
Henrik Austad <henrik@austad.us>
2009-12-01 09:57:37 +0100

    MDB2Wrapper::update documentation to DocBlock format
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- lib/misc/mdb2_wrapper.php
+++ lib/misc/mdb2_wrapper.php
@@ -6,46 +6,53 @@ require_once 'logger.php';
 require_once 'db_query.php';
 require_once 'pw.php';
 
-/* MDB2Wrapper
+/**
+ * MDB2Wrapper	Simple wrapper class for the MDB2-package.
  *
- * Simple wrapper class for the MDB2-package.
- * The class will handle connecting to the database, retrieval of
- * username/passwords etc and return all results in an array.
+ * This is a wrapper to the databasewrapper MDB2. The intention is to completely
+ * remove the need for finding username and password, creating statement etc.
  *
- * Hence, the user is left with a very simple call, just
+ * The goal is to shrink a databasequery down to a *single* line of code elsewhere.
  *
- * $res = MDB2Wrapper::execute(...)
- *
- * where $res will contain the .. result.
- *
- * Author: Henrik Austad <henrik.austad@uninett.no>
+ * @author     Henrik Austad <henrik.austad@uninett.no>
  */
 class MDB2Wrapper
 {
      private static $conn;
      private static $connCounter = 0;
-     /* public static execute()
+
+     /**
+      * execute() execute a sql-query.
       *
-      * params:
-      * query:  prepared statement ready query. i.e. all variables you are going
-      *         to test, leave as ?
-      * types:  the type of the elements in the query.
-      * data:   the actual data to put into the query
+      * A standard sql-query wrapper using prepared statements to minimize the
+      * danger for SQL-injection attacks.
       *
-      * Example:
-      * To retrive the name of all users above a certain age, where the limit is
-      * supplied by a webclient (through POST etc)
+      * It supports both named and unnamed placeholders.
       *
-      * $res = MDB2Wrapper::execute("SELECT name FROM userdb WHERE age > ?",
-      *                                 array('int'),
-      *                                 array('$_GET['age'])");
+      * Named placeholders:
+      *		$data = araray();
+      *		$data['id'] = 1;
+      *		$res = MDB2Wrapper::execute('SELECT * FROM table WHERE id=:id',
+      *					    null,
+      *					    $data);
+      *
+      * Unnamed placeholders:
+      *		$res = MDB2Wrapper::execute('SELECT * FROM table WHERE id==',
+      *					    array('text'),
+      *					    array(1));
+      *
+      * @param String		$query	The query to execute
+      * @param Array|null	$types	The datatypes that goes with the query
+      * @param Array|null	$data	The data to pass along with the query.
+      * @param Boolean		$update	Flag to indicate if the query is an
+      *					update or not. If true, no data will be
+      *					fetched from the resultset.
       */
      public static function execute($query, $types, $data, $update = false)
      {
 
           if (!isset(MDB2Wrapper::$conn))
                MDB2Wrapper::create();
-	  $results = array();
 
           $stmnt = MDB2Wrapper::$conn->prepare($query, $types, MDB2_PREPARE_RESULT);
           if (PEAR::isError($stmnt)) {
@@ -66,26 +73,47 @@ class MDB2Wrapper
           }
           $stmnt->free();
 
-		  $i = 0;
-          while($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
-               $results[$i] = $row;
-               $i = $i+1;
-          }
+	  if ($update) {
+		  return;
+	  }
+
+	  $results = array();
+	  $i = 0;
+	  while($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+		  $results[$i] = $row;
+		  $i = $i+1;
+	  }
 	  MDB2Wrapper::$connCounter += 1;
           return $results;
      } /* end execute */
 
-     /* at the moment, it just uses execute, without returning anything */
+     /**
+      * update()  run an update-query.
+      *
+      * Since this function will only update, nothing is returned. To avoid
+      * adding a lot of code, we hook into execute, and set the update-flag to
+      * true.
+      *
+      * @param String $query the update-query
+      * @param Array|null $types array of types to pass along
+      * @param Array|null $data the data associated with the query.
+      *
+      * @return void
+      * @access public
+      */
      public static function update($query, $types, $data)
      {
 	     MDB2Wrapper::execute($query, $types, $data, true);
      }
 
-    /*
-     * MySQL/the MDB2-MySQL wrapper seems to not support prepared
-     * statements for batch INSERT operations. Hence we have to execute
-     * such and maybe other statements as a query.
-     */
+
+     /**
+      * MDB2Wrapper::query()
+      *
+      * MySQL/the MDB2-MySQL wrapper seems to not support prepared
+      * statements for batch INSERT operations. Hence we have to execute
+      * such and maybe other statements as a query.
+      */
      public static function query($query)
      {
        if (!isset(MDB2Wrapper::$conn)) {
@@ -98,17 +126,17 @@ class MDB2Wrapper
        }
      }
 
-     /* create()
+     /**
+      * MDB2Wrapper::create() create the connection (connect and initialize)
       *
-      * This is where we create the connection (do'h...)
+      * This function will retrieve the database-credentials from the
+      * config-file. From this, it will connect tot he database and store the
+      * connection so the other functions can use it.
       *
-      * It need the config.php (found in lib/) which in turn needs
-      * confusa_config.php with the proper attributes set.
-      * In short, you need config.php, config.php needs confusa_config.php, and
-      * in confusa_config.php, you must have an array called $confusa_config,
-      * with attributes on the form
-      * 'mysql_username' => 'sample_username',
-      * 'mysql...
+      * At the moment, this is hard-coded to mysql (see the naming
+      * below), but if we should ever change to another database, this is the
+      * place to add the logic).
+      *
       */
      private static function create()
      {
@@ -128,9 +156,21 @@ class MDB2Wrapper
                     die(MDB2Wrapper::$conn->getMessage());
                }
 
-          } /* end construct MDB2Wrapper */
+     } /* end create() */
+
+     /**
+      * getConnCounter() return the number of times the connection has been used
+      *
+      * When we are using the database-layer, we want to know how hard we hammer
+      * the database. This function will return the number of queries submitted
+      * to the layer.
+      *
+      * @return Int $connCounter the counter for how many times the wrapper has
+      *				 been used.
+      */
      public static function getConnCounter()
      {
 	     return MDB2Wrapper::$connCounter;
-     }
+     } /* end getConnCounter */
+
 } /* end MDB2Wrapper */
