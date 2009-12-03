4b22c83e682cb398327010fc7d8b64fefe1a5811
Thomas Zangerl <tzangerl@pdc.kth.se>
2009-12-03 15:40:54 +0100

    db_lib.sh: Use dbconfig-configuration, if available
    If there is a dbconfig-configuration on the system, db_lib should actually
    get the DB-connection information from there, not from the Confusa
    configuration.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- lib/bash/db_lib.sh
+++ lib/bash/db_lib.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 # Assumes that the config_lib has been loaded.
 
-username=`get_config_entry "mysql_username"`
-pw=`get_config_entry "mysql_password"`
-host=`get_config_entry "mysql_host"`
-db=`get_config_entry "mysql_db"`
+# if the DB was configured by dbconfig-common, get the connection information
+# from dbconfig-common's configuration file
+if [ -f "/etc/confusa/confusa_config.inc.php" ]; then
+	db_config_file="/etc/confusa/confusa_config.inc.php"
+
+	username=`grep "\\$dbuser=" $db_config_file | cut -d '=' -f 2 \
+		| cut -d "'" -f 2`
+	pw=`grep "\\$dbpass=" $db_config_file | cut -d '=' -f 2 \
+		| cut -d "'" -f 2`
+	host=`grep "\\$dbserver=" $db_config_file | cut -d '=' -f 2 \
+		| cut -d "'" -f 2`
+	db=`grep "\\$dbname=" $db_config_file | cut -d '=' -f 2 \
+		| cut -d "'" -f 2`
+else
+	username=`get_config_entry "mysql_username"`
+	pw=`get_config_entry "mysql_password"`
+	host=`get_config_entry "mysql_host"`
+	db=`get_config_entry "mysql_db"`
+fi
+
 if [ "$pw" == "" ]; then
     pw=""
 else
