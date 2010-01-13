8c59dd2a2ce1b10eb4b2b9fe4230e40bb224c9b4
Henrik Austad <henrik@austad.us>
2010-01-13 18:26:22 +0100

    Confusa_Session: add class for handling sessions
    When we use session-variables to handle data, we should do so in a
    single location and not 'everywhere' in the code.
    
    This adds a new, static class to the code-base for easy manipulation of
    the session store, kept to a confined namespace to avoid collision with
    other variables.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

---
+++ lib/framework/Confusa_Session.php
@@ -0,0 +1,85 @@
+<?php
+
+  /**
+   * CS : Confusa_Session
+   *
+   * This class is a container for the session-variables used within the portal.
+   *
+   * @author Henrik Austad <henrik@austad.us>
+   */
+class CS
+{
+	private static $cs_started = false;
+	private static $name = "CS_Name";
+
+	/**
+	 * testSession() test and set the session to CS_Name
+	 *
+	 * This function is used to make sure that the session is indeed the
+	 * CS_Name, the associated session.
+	 *
+	 * @param  : void
+	 * @return : void
+	 */
+	private static function testSession()
+	{
+		if (session_name() != self::$name) {
+			session_write_close();
+			self::$cs_started = false;
+		}
+
+		if (!self::$cs_started) {
+			self::$cs_started = true;
+			session_name(self::$name);
+			session_start();
+		}
+	}
+
+	/**
+	 * start() start the CS_Name session
+	 *
+	 * @param  : void
+	 * @return : void
+	 */
+	public static function start()
+	{
+		self::testSession();
+	}
+
+	/**
+	 * setSessionKey() take the value and store it in the session under $key
+	 */
+	public static function setSessionKey($key, $value)
+	{
+		self::testSession();
+		$_SESSION[htmlspecialchars($key)] = htmlspecialchars($value);
+	} /* end setSessionKey() */
+
+	public static function getSessionKey($key)
+	{
+		self::testSession();
+		if (isset($_SESSION) && array_key_exists(htmlspecialchars($key), $_SESSION)) {
+			if (isset($_SESSION[htmlspecialchars($key)])) {
+				return htmlspecialchars($_SESSION[htmlspecialchars($key)]);
+			}
+		}
+		return null;
+	} /* end getSessionKey() */
+
+	/**
+	 * dumpSession() dump the content of the session to stdout.
+	 *
+	 * This is only available when debug is enabled.
+	 */
+	public static function dumpSession()
+	{
+		if (Config::get_config('debug')) {
+			self::testSession();
+			echo "<pre>\n";
+			echo "Session name. " . session_name() . "\n";
+			print_r($_SESSION);
+			echo "</pre>\n";
+		}
+	}
+} /* end class CS */
+?>
