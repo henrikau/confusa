e4b8aa9bceff1c9f7388212896df32c21f761ab5
Thomas Zangerl <tzangerl@pdc.kth.se>
2010-01-28 16:35:44 +0100

    eScience/Personal: Restrict e-mail options to '1' XOR 'n' in personal mode
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- www/nren_subs_settings.php
+++ www/nren_subs_settings.php
@@ -7,6 +7,13 @@ require_once 'mdb2_wrapper.php';
 
 class CP_NREN_Subs_Settings extends Content_Page
 {
+
+	private $ESCIENCE_MAILOPTIONS = array('0' => ' None',
+	                                      '1' => ' Single',
+	                                      'n' => ' Multiple');
+	private $PERSONAL_MAILOPTIONS = array('1' => ' Single',
+	                                      'n' => ' Multiple');
+
 	function __construct()
 	{
 		parent::__construct("NREN/subscriber settings", true, "contactinfo");
@@ -32,16 +39,25 @@ class CP_NREN_Subs_Settings extends Content_Page
 					$this->person->getNREN()->set_cert_email(   Input::sanitizeEmail($_POST['cert_email']));
 					$this->person->getNREN()->set_url(          Input::sanitizeURL($_POST['url']));
 					$this->person->getNREN()->set_lang(         Input::sanitizeLangCode($_POST['language']));
+
+					$nren = $this->person->getNREN();
+
 					if (isset($_POST['enable_email'])) {
-						$this->person->getNREN()->setEnableEmail(   Input::sanitizeLangCode($_POST['enable_email']));
+						if (Config::get_config('cert_product') == PRD_PERSONAL) {
+							if (array_key_exists($_POST['enable_email'], $this->PERSONAL_MAILOPTIONS)) {
+								$nren->setEnableEmail($_POST['enable_email']);
+							}
+						} else {
+							if (array_key_exists($_POST['enable_email'], $this->ESCIENCE_MAILOPTIONS)) {
+								$nren->setEnableEmail($_POST['enable_email']);
+							}
+						}
 					}
 
 					if (isset($_POST['cert_validity']) &&
 					    array_search($_POST['cert_validity'], ConfusaConstants::$CAPI_VALID_PERSONAL) !== FALSE) {
 						$this->person->getNREN()->setCertValidity($_POST['cert_validity']);
 					}
-
-					$nren = $this->person->getNREN();
 
 					if ($nren->saveNREN()) {
 						Framework::success_output($this->translateTag('l10n_suc_updatenren', 'contactinfo') . " " .
@@ -88,13 +104,16 @@ class CP_NREN_Subs_Settings extends Content_Page
 		}
 
 		/* export the different subjectAltName email-settings */
-		$this->tpl->assign('enable_options', array('0' => ' None',
-							   '1' => ' Single',
-							   'n' => ' Multiple'));
-		$this->tpl->assign('validity_options', array('365' => ' 365 days',
+		if (Config::get_config('cert_product') === PRD_PERSONAL) {
+			$this->tpl->assign('enable_options', $this->PERSONAL_MAILOPTIONS);
+			$this->tpl->assign('validity_options', array('365' => ' 365 days',
 		                                             '730' => ' 730 days',
 		                                            '1095' => ' 1095 days'));
-		$this->tpl->assign('personal', Config::get_config('cert_product') === PRD_PERSONAL);
+			$this->tpl->assign('personal', true);
+		} else {
+			$this->tpl->assign('enable_options', $this->ESCIENCE_MAILOPTIONS);
+			$this->tpl->assign('personal', false);
+		}
 
 		$this->tpl->assign('languages', $this->full_names);
 		$this->tpl->assign('current_language', $current_language);
