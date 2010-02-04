e5a5f3b0b4fdee47515ab028d494041e9ffcf242
Henrik Austad <henrik@austad.us>
2010-02-04 13:32:43 +0100

    XML_Client: add GPLv3 licensing information to files.
    The files in the library should clearly state that they are released
    under GPLv3 since the entire Confusa is. However, these files are
    expected to be exported as a standalone ZIP-archive, and needs to be
    properly marked.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- extlibs/XML_Client/Confusa_Client.py
+++ extlibs/XML_Client/Confusa_Client.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
-# The HTTPSClientAuthHandler is inspired by
-#       http://www.threepillarsoftware.com/soap_client_auth
 #
+#            Confusa_Client.py is part of Confusa.
+#
+# All of Confusa is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Confusa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Confusa.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 """
