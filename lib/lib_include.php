d6bf86538dc949dd2255fc69995ea89b6522fd68
Henrik Austad <henrik@austad.us>
2010-03-05 14:06:57 +0100

    lib_include: rearrange path-include to alphabetical.
    This should make it simpler to see if a given directory is included or
    not, and where to place it in the file.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- lib/lib_include.php
+++ lib/lib_include.php
@@ -2,15 +2,15 @@
 $path = ini_get('include_path');
 define('LIB_DIR', dirname(__FILE__));
 
+$path .= PATH_SEPARATOR . LIB_DIR . '/actors';
 $path .= PATH_SEPARATOR . LIB_DIR . '/auth';
+$path .= PATH_SEPARATOR . LIB_DIR . '/ca';
 $path .= PATH_SEPARATOR . LIB_DIR . '/db';
 $path .= PATH_SEPARATOR . LIB_DIR . '/exceptions';
 $path .= PATH_SEPARATOR . LIB_DIR . '/file';
-$path .= PATH_SEPARATOR . LIB_DIR . '/ca';
-$path .= PATH_SEPARATOR . LIB_DIR . '/misc';
-$path .= PATH_SEPARATOR . LIB_DIR . '/actors';
 $path .= PATH_SEPARATOR . LIB_DIR . '/framework';
 $path .= PATH_SEPARATOR . LIB_DIR . '/input';
+$path .= PATH_SEPARATOR . LIB_DIR . '/misc';
 $path .= PATH_SEPARATOR . LIB_DIR . '/robot';
 
 if (file_exists('/etc/confusa/confusa_config.php') === true) {
