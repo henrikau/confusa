d0f80228a31bbf695e6ee63a38b54f5666fc2ecc
Thomas Zangerl <tzangerl@pdc.kth.se>
2010-03-05 18:41:21 +0100

    Auth-classes: Move Confusa_Auth[_IdP|_Bypass] to modules
    This should emphasize the modular nature of the AuthHandlers a bit more
    than the current lib-based approach.
    
    Signed-off-by: Thomas Zangerl <tzangerl@pdc.kth.se>

--- lib/lib_include.php
+++ lib/lib_include.php
@@ -1,6 +1,7 @@
 <?php
 $path = ini_get('include_path');
 define('LIB_DIR', dirname(__FILE__));
+define('MODULES_DIR', dirname(LIB_DIR) . '/modules');
 
 $path .= PATH_SEPARATOR . LIB_DIR . '/auth';
 $path .= PATH_SEPARATOR . LIB_DIR . '/exceptions';
@@ -11,6 +12,7 @@ $path .= PATH_SEPARATOR . LIB_DIR . '/actors';
 $path .= PATH_SEPARATOR . LIB_DIR . '/framework';
 $path .= PATH_SEPARATOR . LIB_DIR . '/input';
 $path .= PATH_SEPARATOR . LIB_DIR . '/robot';
+$path .= PATH_SEPARATOR . MODULES_DIR . '/auth';
 
 if (file_exists('/etc/confusa/confusa_config.php') === true) {
     $path .= PATH_SEPARATOR . '/etc/confusa';
