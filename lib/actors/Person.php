henrikau/confusa

commit 2ffd233f54134555d43a158bb386a300bbee2559

Person: do not de-reference subscriber if variable is not set.
If Confusa cannot find an NREN-map for a person, the portal will try to
determine if a person is an NREN-admin. Since the UIDs are now only
unique within a subscriber's realm, the subscriber must be taken into
consideration when querying the database for most admins.

However, NREN-admins do not belong to a subscriber, and the query
handles this. However, the subscriber-variable in person was still
referenced in order to find the database-id (which would be ignored for
NREN-admins).

Before the maps are set, there will be no subscribers, and an NREN-admin
cannot log since PHP will explode once you try to call functions on
null-objects.

This change removes this problem by inserting -1 whenever a subscriber is
null. This value will never be used in the database and will therefore
never result in a false matching to an existing Subscriber. The only
result is that 'nulled' subscriber-references won't kill an NREN-admins
session.

Signed-off-by: Henrik Austad <henrik@austad.us>

lib/actors/Person.php
@@ -823,7 +823,11 @@ class Person{
 		$query .= "((admin_level='2' AND (idp_url='' OR ISNULL(idp_url) OR idp_url=?)) OR";
 		$query .= "(admin_level='1' AND subscriber=?))";
 		$types = array('text', 'text', 'text', 'text');
-		$params = array($this->eppn, $this->nren->getID(), $this->nren->getIdP(), $this->subscriber->getDBID());
+		$params = array($this->eppn,
+				$this->nren->getID(),
+				$this->nren->getIdP(),
+				(!is_null($this->getSubscriber() ?
+					  $this->getSubscriber()->getDBID() : -1)));
 
 	    $res	= MDB2Wrapper::execute($query, $types, $params);
 	    $size	= count($res);

commit e681f15426fc370c5d0fcfbd6383e97a7c0d0884

Person: PHP does not handle the conditional operator (?) correctly
The error was introduced in commit 2ffd233f, and failed to be detected
in the testing as the only subscriber in the testrig happened to have
id=1

See http://en.wikipedia.org/wiki/Conditional_operator#PHP for details.

This change moves the query over to named expressions and uses an
explicit if-test to see if the subscriber is set. The code is a bit more
cumbersome and not as elegant as before, but it provides the correct
output.

Signed-off-by: Henrik Austad <henrik@austad.us>

lib/actors/Person.php
@@ -819,17 +819,19 @@ class Person{
 	    require_once 'MDB2Wrapper.php';
 	    $errorCode = PW::create(8);
 
-		$query = "SELECT * FROM admins WHERE admin=? AND nren=? AND ";
-		$query .= "((admin_level='2' AND (idp_url='' OR ISNULL(idp_url) OR idp_url=?)) OR";
-		$query .= "(admin_level='1' AND subscriber=?))";
-		$types = array('text', 'text', 'text', 'text');
-		$params = array($this->eppn,
-				$this->nren->getID(),
-				$this->nren->getIdP(),
-				(!is_null($this->getSubscriber() ?
-					  $this->getSubscriber()->getDBID() : -1)));
+	    $query = "SELECT * FROM admins WHERE admin=:admin AND nren=:nren_id AND ";
+	    $query .= "((admin_level='2' AND (idp_url='' OR ISNULL(idp_url) OR idp_url=:idp_url)) OR ";
+	    $query .= "(admin_level='1' AND subscriber=:subscriber_id))";
+	    $params = array();
+	    $params['admin'] = $this->eppn;
+	    $params['nren_id'] = $this->nren->getID();
+	    $params['idp_url'] = $this->nren->getIdP();
+	    $params['subscriber_id'] = -1;
+	    if (!is_null($this->getSubscriber())) {
+		    $params['subscriber_id'] = $this->getSubscriber()->getDBID();
+	    }
 
-	    $res	= MDB2Wrapper::execute($query, $types, $params);
+	    $res	= MDB2Wrapper::execute($query, null, $params);
 	    $size	= count($res);
 	    if ($size == 1) {
 		    $adminRes = $res[0]['admin_level'];