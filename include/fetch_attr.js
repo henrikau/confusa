c6d036ad07e7789a36fefafe8a9e54cd48da6836
Henrik Austad <henrik@austad.us>
2010-05-27 13:25:27 +0200

    Attributes: move javascript to separate file.
    When we include javascript in the page, use the rawScript hook to make
    the inclusion a bit prettier.
    
    This also helps structure the scripts and make up for a more generic approach.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

---
+++ include/fetch_attr.js
@@ -0,0 +1,37 @@
+<script type="text/javascript">
+    <!-- Ask the stylist for the value of the attribute key	-->
+    <!-- currently selected in 'selectElement' with a polite 	-->
+    <!-- asynchronous GET message					-->
+    <!--  -->
+    <!-- @param	selectElement DOM-Node	The <select> containing	-->
+    <!--					the selected attribute	-->
+    <!--					key			-->
+    <!-- @param	targetElementID string	The ID of the element	-->
+    <!--					where the value should	-->
+    <!--					be written to		-->
+    <!-- @param	errMsg string: A string that will be displayed if the -->
+    <!--						attribute value can not be fetched -->
+    <!-- @return	void						-->
+    function fetchAttributeValue(selectElement, targetElementID,  errMsg)
+{
+    var req   = new XMLHttpRequest();
+    var field = document.getElementById(targetElementID);
+    var path  = "?attr_value=" + selectElement.value;
+
+    req.open("GET", path, true);
+
+    req.send(null);
+    req.onreadystatechange = function() {
+    	if (req.readyState == 4 /*complete*/) {
+    	    if (req.status == 200) {
+    		if (req.responseText.length > 0) {
+    		    field.innerHTML = req.responseText;
+    		    field.title = req.responseText;
+    		}
+    	    } else {
+    		field.innerHTML = errMsg;
+    	    }
+    	}
+    }}
+
+</script>
