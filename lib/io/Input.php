8590aeb37e77d9ac73f32aad7dfa0c76db0a5d5b
Henrik Austad <henrik@austad.us>
2010-06-10 14:57:17 +0200

    Input: allow '\=,' in common-name (as CN may be the complete \DN)
    When we query Comodo, we risk getting the entire DN back as a
    result. The DN contains all the fields (e.g. \C, \CN) and if we strip
    too enthusiastically, we loose the ability to revoke certificates.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- lib/io/Input.php
+++ lib/io/Input.php
@@ -140,7 +140,7 @@ class Input
 	 */
 	static function sanitizeCommonName($input)
 	{
-		$output = mb_ereg_replace('[^[:alpha:]\s\.0-9@_\-\+]', '', $input, 'ip');
+		$output = mb_ereg_replace('[^[:alpha:]\s\.0-9@_\-\+=/,]', '', $input, 'ip');
 		return $output;
 	}
 
