6028821b9c37ec595dfd153b005a7dff04db713c
Henrik Austad <henrik@austad.us>
2011-05-31 00:07:18 +0200

    Tests: update test-suite.
    Still a long way to go, but when we change the Input-functions, we
    should make sure the tests are updated to avoid strange sanitazion-bugs
    int he future.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- tests/Test_Input.php
+++ tests/Test_Input.php
@@ -18,7 +18,43 @@ class Test_Input extends Test
 		$res=true;
 		$res &= $this->testURL();
 		$res &= $this->testSanitizeDiff();
+		$res &= $this->testEmail();
+		$res &= $this->testNumbers();
+		$res &= $this->testOrgName();
+		$res &= $this->testACSRF();
+		return $res;
+	}
 
+	private function testACSRF()
+	{
+		$res = true;
+		$t = "123:".sha1("123"."hello world");
+		$res &= Input::sanitizeAntiCSRFToken($t) == $t;
+		$res &= !(Input::sanitizeAntiCSRFToken($t) == "x:meh");
+		$res &= !(Input::sanitizeAntiCSRFToken("134:hello world") == $t);
+		return $res;
+	}
+	private function testOrgName()
+	{
+		$res = true;
+		/* $res &= Input::sanitizeOrgName("test"); */
+
+		if (!$res)
+			$this->printMsg("Failed sanitizing of orgname");
+		return $res;
+	}
+
+	private function testNumbers()
+	{
+		$res = true;
+		$res &= Input::sanitizeNumeric(10) == 10;
+		$res &= Input::sanitizeNumeric("10") == 10;
+		$res &= Input::sanitizeNumeric(-3) == -3;
+		$res &= Input::sanitizeNumeric(380243) == 380243;
+		$res &= !Input::sanitizeNumeric("hello world") == "hello world";
+		$res &= !Input::sanitizeNumeric("x10") == "x10";
+		if (!$res)
+			$this->printMsg("failed sanitizing numbers");
 		return $res;
 	}
 
@@ -28,6 +64,33 @@ class Test_Input extends Test
 		$url      = "https://meh.idp.org/whatever/meh.php?var=foo";
 		if (Input::sanitizeURL($url_ugly) !== $url)
 			return false;
+		return true;
+	}
+
+	private function testEmail()
+	{
+		$emails_ok = array("dclo@us.ibm.com",
+				   "A12345@example.com",
+				   "john.doe@example.com",
+				   "user+mailbox@example.com",
+				   "peter.piper@example.com");
+
+		$emails_nok = array("hello world@example.com" => "helloworld@example.com");
+
+
+		foreach($emails_ok as $email) {
+			if (Input::sanitizeEmail($email) != $email) {
+				$this->printMsg("Input failed on sanitizing $email");
+				return false;
+			}
+		}
+
+		foreach($emails_nok as $email => $correct) {
+			if (trim(Input::sanitizeEmail($email)) != trim($correct)) {
+				$this->printMsg("Input failed on sanitizing $email -> expected $correct, got " . Input::sanitizeEmail($email) . ".");
+				return false;
+			}
+		}
 		return true;
 	}
 
