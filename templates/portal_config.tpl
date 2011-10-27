e79e985aa164478139252314ac22ba5933f1d95c
Henrik Austad <henrik@austad.us>
2011-10-28 10:28:00 +1300

    NREN-maint: set mode and message for maintenance-mode
    This change will
    - change the maintenance-mode through the GUI
    - update the maintenance-message through the GUI
    - log changes to both mode and message
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- templates/portal_config.tpl
+++ templates/portal_config.tpl
@@ -1,8 +1,46 @@
 <h4>{$l10n_portal_config_title}</h4>
 
 <fieldset>
+  <legend>{$l10n_nren_maint_mode_header}</legend>
+  <br />
+  <p class="info">{$l10n_nren_maint_mode_info}</p>
+
+  {if isset($maint_mode)}
+  <p class="info">
+  {if $maint_mode}
+  {$l10n_nren_maint_mode_enabled}
+  {else}
+  {$l10n_nren_maint_mode_disabled}
+  {/if}
+  </p>
+  {/if}
+
+  <form method="post" action="">
+    {$panticsrf}
+    <label name="maint_mode">Enabled </label>
+    <input type="radio"
+	   name="nren_maint_mode"
+	   value="y"
+	   {if $maint_mode}checked="checked"{/if}
+	   />
+    <label name="maint_mode">Disabled </label>
+    <input type="radio"
+	   name="nren_maint_mode"
+	   value="n"
+	   {if ! $maint_mode}checked="checked"{/if}
+	   />
+    <input type="submit" value="update" />
+  </form>
+  <br />
+</fieldset>
+
+<br />
+<fieldset>
   <legend>{$l10n_nren_maint_legend}</legend>
-  <p class="info">{$l10n_nren_maint_msg}</p>
+  <br />
+  <p class="info">
+    {$l10n_nren_maint_msg}
+  </p>
   <hr /><br />
   <p class="info">{$l10n_nren_maint_toggle}</p>
 
@@ -13,6 +51,7 @@
 	      cols="70">{if isset($nren_maint_msg)}{$nren_maint_msg|escape}{/if}</textarea>
     <br /><br />
     <input type="submit" value="{$l10n_nren_maint_update_msg}" />
+    <br />
   </form>
   <br />
   <hr /><br />
--- www/portal_config.php
+++ www/portal_config.php
@@ -6,37 +6,56 @@ require_once 'Framework.php';
 class CP_Portal_Config extends Content_Page
 {
 	private $maint_msg		= null;
+	private $maint_mode		= null;
 
 	function __construct() {
 		parent::__construct("Portal Config", true, 'portal_config');
 	}
 
+
 	public function pre_process($person)
 	{
 		parent::pre_process($person);
+		$this->readMaintMsg();
+		$this->readMaintMode();
 
 		/* if $person is not a NREN admin we stop here */
 		if (!$this->person->isNRENAdmin()) {
 			return false;
 		}
 
-		/* get existing maint-msg */
-		$this->getMaintMsg();
 
 		/* set new maint-msg */
 		if (array_key_exists("nren_maint_msg", $_POST)
 		    && $_POST["nren_maint_msg"] !== ""
 		    && $_POST["nren_maint_msg"] !== $this->maint_msg) {
-			try {
-				$this->saveMaintMsg($_POST['nren_maint_msg']);
+			if ($this->saveMaintMsg($_POST['nren_maint_msg'])) {
 				Framework::success_output("Successfully saved new NREN maintenance message to database.");
-			} catch(Exception $e) {
-				$this->fw->error_output("Could not save NREN maintenance message to database.");
+			} else {
+				Framework::error_output("Could not save NREN maintenance message to database.");
 			}
 		}
 
 		/* set/unset in maint-mode? */
-	}
+		if (array_key_exists("nren_maint_mode", $_POST)) {
+			$mode = $_POST['nren_maint_mode'];
+			if ($this->maint_mode !== $mode) {
+				if ($this->saveMaintMode(Input::sanitizeMaintMode($mode))) {
+					if ($this->maint_mode === 'y')
+						Framework::success_output("Successfully placed portal in NREN-maintenance mode.");
+					else
+						Framework::success_output("Successfully placed portal in NREN-normal mode.");
+				} else {
+					if ($this->maint_mode === 'y')
+						Framework::error_output("Could not place portal in NREN-maintenance-mode.");
+					else
+						Framework::error_output("Could not place portal in NREN-normal mode.");
+
+				}
+			}
+
+		}
+	} /* end pre_process() */
 
 	public function process()
 	{
@@ -50,10 +69,14 @@ class CP_Portal_Config extends Content_Page
 		if (!is_null($this->maint_msg))
 			$this->tpl->assign('nren_maint_msg', $this->maint_msg);
 
+		$this->tpl->assign('maint_mode', ($this->maint_mode === 'y') ? true : false);
 		$this->tpl->assign('content', $this->tpl->fetch('portal_config.tpl'));
 	}
 
-	private function getMaintMsg()
+	/**
+	 * readMaintMsg() read the message from the database and cache locally.
+	 */
+	private function readMaintMsg()
 	{
 		if (!is_null($this->maint_msg))
 			return false;
@@ -63,8 +86,10 @@ class CP_Portal_Config extends Content_Page
 						     array('text'),
 						     array($this->person->getNREN()->getID()));
 		} catch (DBQueryException $dbqe) {
+			/* FIXME */
 			;
 		} catch (DBStatementException $dbse) {
+			/* FIXME */
 			;
 		}
 		if (count($res) == 1) {
@@ -74,24 +99,105 @@ class CP_Portal_Config extends Content_Page
 			}
 		}
 		return false;
-	} /* end getMaintMsg() */
+	} /* end readMaintMsg() */
 
 	private function saveMaintMsg($msg)
 	{
-		if (is_null($this->maint_msg))
-			$this->getMaintMsg;
-		if (is_null($this->maint_msg))
-			throw new Exception("Could not retrieve maint-msg from DB. Cannot save new msg");
-		$this->maint_msg = $msg;
-		MDB2Wrapper::update("UPDATE nrens SET maint_msg=?  WHERE nren_id=?",
-				    array('text', 'text'),
-				    array($msg, $this->person->getNREN()->getID()));
-
-		$this->maint_msg = null;
-		$this->getMaintMsg();
-		if ($this->maint_msg !== $msg) {
-			throw new Exception("Error when updating nren-maint-msg, could not write correct msg to database");
+		if (!isset($msg))
+			return false;
+		try {
+			MDB2Wrapper::update("UPDATE nrens SET maint_msg=?  WHERE nren_id=?",
+								array('text', 'text'),
+								array($msg, $this->person->getNREN()->getID()));
+		} catch (DBQueryException $dbqe) {
+			/* FIXME */
+			;
+		} catch (DBStatementException $dbse) {
+			/* FIXME */
+			;
 		}
+
+		$this->readMaintMsg();
+		$nname = $this->person->getNREN()->getName();
+		if ($this->maint_msg !== $msg) {
+			Logger::log_event(LOG_ERR, "Could not save NREN-maintenance-message for $nname to DB.");
+			return false;
+		}
+		Logger::log_event(LOG_NOTICE, $this->person->getEPPN() . "(".
+						  $this->person->getName().
+						  ") updated maintenance-message for " .
+						  $this->person->getNREN()->getName());
+		return true;
+	}
+
+	/**
+	 * readMaintMode() - return the mode
+	 *
+	 * This function is pretty strict, only 'y' will evaluate to be in
+	 * maintenance-mode, everyting else will result in 'n' (also if value is
+	 * empty or erroneous.
+	 *
+	 * @param void
+	 * @return void
+	 * @access private
+	 */
+	private function readMaintMode()
+	{
+		if (isset($this->maint_mode)) {
+			return;
+		}
+		try {
+			$res = MDB2Wrapper::execute("SELECT maint_mode FROM nrens WHERE nren_id=?",
+										array('text'),
+										array($this->person->getNREN()->getID()));
+		} catch (DBQueryException $dbqe) {
+			/* FIXME */
+			;
+		} catch (DBStatementException $dbse) {
+			/* FIXME */
+			;
+		}
+		$this->maint_mode = 'n';
+		if (count($res) > 0 && array_key_exists('maint_mode', $res[0])) {
+			$this->maint_mode = Input::sanitizeMaintMode($res[0]['maint_mode']);
+		}
+	}
+
+	/**
+	 * setMaintMode() - set the maintenance-mode for the current NREN
+	 *
+	 * @param String $mode the mode (either 'y' or 'n')
+	 * @returns Boolean success if the new mode was stored in the database.
+	 * @access private
+	 */
+	private function saveMaintMode($mode)
+	{
+		if (!isset($mode))
+			return false;
+		unset($this->maint_mode);
+		try {
+			MDB2Wrapper::update("UPDATE nrens SET maint_mode=? WHERE nren_id=?",
+								array('text', 'text'),
+								array($mode, $this->person->getNREN()->getID()));
+		} catch (DBQueryException $dbqe) {
+			/* FIXME */
+			;
+		} catch (DBStatementException $dbse) {
+			/* FIXME */
+			;
+		}
+		$this->readMaintMode();
+		$nname = $this->person->getNREN()->getName();
+
+		if ($this->maint_mode !== $mode) {
+			Logger::log_event(LOG_ERR, "Could not update maintenance-mode for $nname in the database.");
+			return false;
+		}
+
+		Logger::log_event(LOG_NOTICE, $this->person->getEPPN() . " (".
+						  $this->person->getName().
+						  ") moved NREN $nname" .
+						  ($mode === 'y' ? " into maintenance mode." : " out of maintenance mode."));
 		return true;
 	}
 }
