4fdb34ebcb68009a7369e9ae50d73d061b2a5c6d
Henrik Austad <henrik@austad.us>
2011-12-08 22:39:50 +0100

    NREN Settings: start cleanup of info
    When we add/update info, we should be a bit more flexible when it comes
    to saving it. This will nudge nren_subs_settings in the direction
    add_nren.php has taken.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- www/nren_subs_settings.php
+++ www/nren_subs_settings.php
@@ -14,12 +14,23 @@ class CP_NREN_Subs_Settings extends Content_Page
 	                                      'm' => ' Multiple, at least one.');
 	private $PERSONAL_MAILOPTIONS = array('1' => ' Single',
 	                                      'm' => ' Multiple, at least one.');
-
+	private $form_data;
+	private $validation_error;
 	function __construct()
 	{
 		parent::__construct("NREN/subscriber settings", true, "contactinfo");
 		$available_languages = Config::get_config('language.available');
 		$this->full_names = Translator::getFullNamesForISOCodes($available_languages);
+
+		$this->form_data['contact_email'] = "";
+		$this->form_data['contact_phone'] = "";
+		$this->form_data['sanitizedCertPhone'] = "";
+		$this->form_data['sanitizedCertEmail'] = "";
+		$this->form_data['sanitizedURL'] = "";
+		$this->form_data['sanitizedWAYF'] = "";
+		$this->form_data['enable_email'] = "";
+		$this->form_data['cert_validity'] = "";
+		$this->validation_error = false;
 	}
 
 	public function pre_process($person)
@@ -27,110 +38,114 @@ class CP_NREN_Subs_Settings extends Content_Page
 		parent::pre_process($person);
 
 		/* IF user is not subscirber- or nren-admin, we stop here */
-		if (!($this->person->isSubscriberAdmin() || $this->person->isNRENAdmin()))
+		if (!($this->person->isSubscriberAdmin() || $this->person->isNRENAdmin())) {
 			return false;
+		}
 
 		if (isset($_POST['setting'])) {
 			switch($_POST['setting']) {
 			case 'nren_contact':
 				if ($this->person->isNRENAdmin()) {
 
-					$sanitizedEmail =
-						Input::sanitizeEmail($_POST['contact_email']);
-					$sanitizedContactPhone =
-						Input::sanitizePhone($_POST['contact_phone']);
-					$sanitizedCertPhone =
-						Input::sanitizePhone($_POST['cert_phone']);
-					$sanitizedCertEmail =
-						Input::sanitizeEmail($_POST['cert_email']);
-					$sanitizedURL = Input::sanitizeURL($_POST['url']);
-					$sanitizedWAYF = Input::sanitizeURL($_POST['wayf_url']);
-
-					$validationError = false;
-
-					if ($_POST['contact_email'] != $sanitizedEmail) {
-						$this->displayInvalidCharError($_POST['contact_email'],
-						                               $sanitizedEmail,
-						                               'l10n_label_contactemail');
-						$validationError = true;
+					if (array_key_exists('contact_email', $_POST)) {
+						$this->form_data['contact_email'] = Input::sanitizeEmail($_POST['contact_email']);
+						if ($_POST['contact_email'] !== $this->form_data['contact_email']) {
+							$this->displayInvalidCharError($_POST['contact_email'],
+														   $_POST['contact_email'],
+														   'l10n_label_contactemail');
+							$this->form_data['contact_email'] = "";
+							$this->validationError = true;
+						}
 					}
 
-					if ($_POST['contact_phone'] != $sanitizedContactPhone) {
-						$this->displayInvalidCharError($_POST['contact_phone'],
-						                               $sanitizedContactPhone,
-						                               'l10n_label_contactphone');
-						$validationError = true;
+					if (array_key_exists('contact_phone', $_POST)) {
+						$this->form_data['contact_phone'] = Input::sanitizePhone($_POST['contact_phone']);
+						if ($_POST['contact_phone'] !== $this->form_data['contact_phone']) {
+							$this->displayInvalidCharError($_POST['contact_phone'],
+														   $this->form_data['contact_phone'],
+														   'l10n_label_contactphone');
+							$this->form_data['contact_phone'] = "";
+							$this->validationError = true;
+						}
 					}
 
-					if ($_POST['cert_phone'] != $sanitizedCertPhone) {
-						$this->displayInvalidCharError($_POST['cert_phone'],
-						                               $sanitizedCertPhone,
-						                               'l10n_label_certphone');
-						$validationError = true;
+					if (array_key_exists('cert_phone', $_POST)) {
+						$this->form_data['sanitizedCertPhone'] =Input::sanitizePhone($_POST['cert_phone']);
+						if ($_POST['cert_phone'] != $this->form_data['sanitizedCertPhone']) {
+							$this->displayInvalidCharError($_POST['cert_phone'],
+														   $this->form_data['sanitizedCertPhone'],
+														   'l10n_label_certphone');
+							$this->form_data['sanitizedCertPhone'] = "";
+							$this->validationError = true;
+						}
 					}
 
-					if ($_POST['cert_email'] != $sanitizedCertEmail) {
-						$this->displayInvalidCharError($_POST['cert_email'],
-						                               $sanitizedCertEmail,
-						                               'l10n_label_certmail');
-						$validationError = true;
+					if (array_key_exists('cert_email', $_POST)) {
+						$this->form_data['sanitizedCertEmail'] = Input::sanitizeEmail($_POST['cert_email']);
+						if ($_POST['cert_email'] != $this->form_data['sanitizedCertEmail']) {
+							$this->displayInvalidCharError($_POST['cert_email'],
+														   $this->form_data['sanitizedCertEmail'],
+														   'l10n_label_certmail');
+							$this->form_data['sanitizedCertEmail'] = "";
+							$this->validationError = true;
+						}
 					}
 
-					if ($_POST['url'] != $sanitizedURL) {
-						$this->displayInvalidCharError($_POST['url'],
-						                               $sanitizedURL,
-						                               'l10n_label_nrenurl');
-						$validationError = true;
+					if (array_key_exists('url', $_POST)) {
+						$this->form_data['sanitizedURL'] = Input::sanitizeURL($_POST['url']);
+						if ($_POST['url'] != $this->form_data['sanitizedURL']) {
+							$this->displayInvalidCharError($_POST['url'],
+														   $this->form_data['sanitizedURL'],
+														   'l10n_label_nrenurl');
+							$this->form_data['sanitizedURL'] = "";
+							$this->validationError = true;
+						}
 					}
 
-					if ($_POST['wayf_url'] != $sanitizedWAYF) {
-						$this->displayInvalidCharError($_POST['wayf_url'],
-						                               $sanitizedWAYF,
-						                               'l10n_label_wayfurl');
-						$validationError = true;
+					if (array_key_exists('wayf_url', $_POST)) {
+						$this->form_data['sanitizedWAYF'] = Input::sanitizeURL($_POST['wayf_url']);
+						if ($_POST['wayf_url'] != $this->form_data['sanitizedWAYF']) {
+							$this->displayInvalidCharError($_POST['wayf_url'],
+														   $this->form_data['sanitizedWAYF'],
+														   'l10n_label_wayfurl');
+							$this->form_data['sanitizedWAYF'] = "";
+							$this->validationError = true;
+						}
 					}
 
-					/* don't continue if information has been stripped */
-					if ($validationError) {
-						return;
-					}
-
-					$this->person->getNREN()->setContactEmail( Input::sanitizeEmail($_POST['contact_email']));
-					$this->person->getNREN()->setContactPhone( $sanitizedContactPhone);
-					$this->person->getNREN()->setCertPhone(    $sanitizedCertPhone);
-					$this->person->getNREN()->setCertEmail(    $sanitizedCertEmail);
-					$this->person->getNREN()->setURL(          $sanitizedURL);
-					$this->person->getNREN()->setLang(         Input::sanitizeLangCode($_POST['language']));
-					if (!$this->person->getNREN()->setWAYFURL( $sanitizedWAYF)) {
-						Framework::error_output($this->translateTag('l10n_error_wayf_url', 'contactinfo'));
-					}
-					$this->person->getNREN()->setReauthTimeout(Input::sanitizeNumeric($_POST['reauth_timeout']));
-
-					$nren = $this->person->getNREN();
-
-					if (isset($_POST['enable_email'])) {
+					if (array_key_exists('enable_email', $_POST) && isset($_POST['enable_email'])) {
 						if (Config::get_config('cert_product') == PRD_PERSONAL) {
 							if (array_key_exists($_POST['enable_email'], $this->PERSONAL_MAILOPTIONS)) {
-								$nren->setEnableEmail($_POST['enable_email']);
+								$this->form_data['enable_email'] = $_POST['enable_email'];
 							}
 						} else {
 							if (array_key_exists($_POST['enable_email'], $this->ESCIENCE_MAILOPTIONS)) {
-								$nren->setEnableEmail($_POST['enable_email']);
+								$this->form_data['enable_email'] = $_POST['enable_email'];
 							}
 						}
 					}
 
+					if (array_key_exists('reauth_timeout', $_POST) && isset($_POST['reauth_timeout'])) {
+						$this->form_data['reauth_timeout'] = Input::sanitizeNumeric($_POST['reauth_timeout']);
+					}
 					if (isset($_POST['cert_validity']) &&
 					    array_search($_POST['cert_validity'], ConfusaConstants::$CAPI_VALID_PERSONAL) !== FALSE) {
-						$this->person->getNREN()->setCertValidity($_POST['cert_validity']);
+						$this->form_data['cert_validity'] = $_POST['cert_validity'];
 					}
 
-					if ($nren->saveNREN()) {
-						Framework::success_output($this->translateTag('l10n_suc_updatenren', 'contactinfo') . " " .
-						                          $nren->getName());
+
+					/* don't continue if information has been stripped */
+					if ($this->validation_error) {
+						return;
 					}
+					if ($this->updateNRENContact()) {
+						Framework::success_output($this->translateTag('l10n_suc_updatenren', 'contactinfo') . " " .
+						                          $this->person->getNREN()->getName());
+					}
+
 				}
 				break;
+
 			case 'subscriber_contact':
 				if ($this->person->isSubscriberAdmin()) {
 					$sanitizedMail =
@@ -146,54 +161,54 @@ class CP_NREN_Subs_Settings extends Content_Page
 					$sanitizedHelpdeskMail =
 						Input::sanitizeEmail($_POST['helpdesk_email']);
 
-					$validationError = false;
+					$this->validationError = false;
 
 					if ($_POST['contact_email'] != $sanitizedMail) {
 						$this->displayInvalidCharError($_POST['contact_email'],
 						                               $sanitizedMail,
 						                               'l10n_label_contactemail');
-						$validationError = true;
+						$this->validationError = true;
 					}
 
 					if ($_POST['contact_phone'] != $sanitizedPhone) {
 						$this->displayInvalidCharError($_POST['contact_phone'],
 						                              $sanitizedPhone,
 						                              'l10n_label_contactphone');
-						$validationError = true;
+						$this->validationError = true;
 					}
 
 					if ($_POST['resp_name'] != $sanitizedRespName) {
 						$this->displayInvalidCharError($_POST['resp_name'],
 						                               $sanitizedRespName,
 						                               'l10n_label_respname');
-						$validationError = true;
+						$this->validationError = true;
 					}
 
 					if ($_POST['resp_email'] != $sanitizedRespMail) {
 						$this->displayInvalidCharError($_POST['resp_email'],
 						                               $sanitizedRespMail,
 						                               'l10n_label_respemail');
-						$validationError = true;
+						$this->validationError = true;
 					}
 
 					if ($_POST['helpdesk_url'] != $sanitizedHelpdeskURL) {
 						$this->displayInvalidCharError($_POST['helpdesk_url'],
 						                               $sanitizedHelpdeskURL,
 						                               'l10n_label_helpdeskurl');
-						$validationError = true;
+						$this->validationError = true;
 					}
 
 					if ($_POST['helpdesk_email'] != $sanitizedHelpdeskMail) {
 						$this->displayInvalidCharError($_POST['helpdesk_email'],
 						                               $sanitizedHelpdeskMail,
 						                               'l10n_label_helpemail');
-						$validationError = true;
+						$this->validationError = true;
 					}
 
 					/*
 					 * don't continue if data got stripped
 					 */
-					if ($validationError) {
+					if ($this->validationError) {
 						return;
 					}
 
@@ -250,6 +265,52 @@ class CP_NREN_Subs_Settings extends Content_Page
 		$this->tpl->assign('content', $this->tpl->fetch('nren_subs_settings.tpl'));
 	}
 
+	private function updateNRENContact()
+	{
+		$nren = $this->person->getNREN();
+		if (!isset($nren) || $this->validation_error) {
+			return false;
+		}
+		if (isset($this->form_data['contact_email']) &&
+			$this->form_data['contact_email'] !== "") {
+			$nren->setContactEmail($this->form_data['contact_email']);
+		}
+
+		if (isset($this->form_data['contact_phone']) &&
+			$this->form_data['contact_phone'] !== "") {
+			$nren->setContactPhone($this->form_data['contact_phone']);
+		}
+		if (isset($this->form_data['sanitizedCertPhone']) &&
+			$this->form_data['sanitizedCertPhone'] !== "") {
+			$nren->setCertPhone($this->form_data['sanitizedCertPhone']);
+		}
+		if (isset($this->form_data['sanitizedCertEmail']) &&
+			$this->form_data['sanitizedCertEmail'] !== "") {
+			$nren->setCertEmail($this->form_data['sanitizedCertEmail']);
+		}
+		if (isset($this->form_data['sanitizedURL']) &&
+			$this->form_data['sanitizedURL'] !== "") {
+			$nren->setURL($this->form_data['sanitizedURL']);
+		}
+		if (isset($this->form_data['sanitizedWAYF']) &&
+			$this->form_data['sanitizedWAYF'] !== "") {
+			$nren->setWAYFURL($this->form_data['sanitizedWAYF']);
+		}
+		if (isset($this->form_data['enable_email']) &&
+			$this->form_data['enable_email'] !== "") {
+			$nren->setEnableEmail($this->form_data['enable_email']);
+		}
+		if (isset($this->form_data['cert_validity']) &&
+			$this->form_data['cert_validity'] !== "") {
+			$nren->setCertValidity($this->form_data['cert_validity']);
+		}
+		if (isset($this->form_data['reauth_timeout']) &&
+			$this->form_data['reauth_timeout'] !== "") {
+			$this->person->getNREN()->setReauthTimeout($this->form_data['reauth_timeout']);
+		}
+
+		return $nren->saveNREN();
+	}
 	/**
 	 * Update the contact information for a subscriber to a new value
 	 *
@@ -262,13 +323,7 @@ class CP_NREN_Subs_Settings extends Content_Page
 	 * @param $language string the language code for the subscriber's preferred
 	 *                         language
 	 */
-	private function updateSubscriberContact($contact_email,
-	                                         $contact_phone,
-	                                         $resp_name,
-	                                         $resp_email,
-	                                         $help_url,
-	                                         $help_email,
-	                                         $language)
+	private function updateSubscriberContact($language)
 	{
 		$subscriber = $this->person->getSubscriber();
 		$subscriber->setEmail($contact_email);
