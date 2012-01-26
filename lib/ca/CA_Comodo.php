c5be6f9ecfd526bcebb20162ee62d061723a0d75
Henrik Austad <henrik@austad.us>
2012-01-26 21:57:46 +0100

    CA_Comodo: add wildcard at the head of commonName
    This is needed in order to actually find _any_ certificates belonging to
    the user. It was mistakenly removed in commit 97333236.
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- lib/ca/CA_Comodo.php
+++ lib/ca/CA_Comodo.php
@@ -795,7 +795,7 @@ class CA_Comodo extends CA
 			/* be sure to match the EPPN only and not also its suffix. For
 			 * instance test@feide.no should not match confusatest@feide.no
 			 */
-			$postfields_list["commonName"] = $eppn;
+			$postfields_list["commonName"] = "% " . $eppn;
 		} else if (Config::get_config('cert_product') == PRD_PERSONAL) {
 			$postfields_list["unstructuredName"] = $eppn;
 		} else {
