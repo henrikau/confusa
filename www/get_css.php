f099e0595206e1cff298b13d416b08250ca2e291
Thijs Kinkhorst <thijs@uvt.nl>
2012-01-30 11:33:37 +0100

    move text/css header output up to also emit the header for empty content.
    If the NREN-custom css is empty, the document was sent with the default
    content type text/html. This causes a warning in the browser console:
      The stylesheet ... was not loaded because its MIME type, "text/html",
      is not "text/css".
    
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- www/get_css.php
+++ www/get_css.php
@@ -6,12 +6,13 @@ require_once 'Input.php';
 $nren = Input::sanitizeNRENName($_GET['nren']);
 $css_path = Config::get_config('custom_css') . $nren . '/custom.css';
 
+header("Content-type: text/css");
+
 if (file_exists($css_path)) {
 	$fp = fopen($css_path, "r");
 	$css = fread($fp, filesize($css_path));
 	fclose($fp);
 
-	header("Content-type: text/css");
 	echo $css;
 } else {
 	echo "";
