e0ba78a796c2c39a87d5571c4580a70b5cdbb10d
Henrik Austad <henrik@austad.us>
2012-03-06 22:43:59 +0100

    tests: update CSR-tests
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- tests/test_csr_upload.php
+++ tests/test_csr_upload.php
@@ -4,9 +4,20 @@ require_once "exceptions.php";
 require_once "../lib/io/Input.php";
 require_once "../lib/file/CSRUpload.php";
 
-/* Files containing compromised RSA-keys
+/* Getting the compromised CSR-test to work
+ *
+ * Download either (or both) archived containing compromised RSA-keys:
  * http://research.naumachiarius.com/download/inkey-OSSL-4096-X86-NOR-NEW-XXXX-REG.tar.gz
- * http://research.naumachiarius.com/download/inkey-OSSL-3072-X86-NOR-OLD-XXXX-REG.tar.gz
+ *
+ * Unzip to <confusa_dir>/tests/files
+ * Convert all files in this directory to PEM (will take a few minutes to complete):
+ *
+ * for i in $(ls); do \
+ *		openssl rsa -inform DER -outform PEM -in $i -out $i.pem; \
+ *		rm -fv $i; \
+ *		done
+ *
+ * Now it should be ready to run
  */
 class TestCSRUpload extends UnitTestCase
 {
@@ -16,29 +27,37 @@ class TestCSRUpload extends UnitTestCase
 		;						/* fixme */
 	}
 
-	function testBlacklist()
+	function testBlacklistWithGood()
 	{
-		$csr = $this->genCSR();
-		$this->assertFalse(empty($csr));
-		try {
-			CSRUpload::testBlacklist($csr);
-			$this->pass();
-		} catch (Exception  $e) {
-			$this->fail("newly generated CSR should not result in an exception.");
+		$c = 2;
+		while($c-- > 0) {
+			$csr = $this->genCSR();
+			$this->assertFalse(empty($csr));
+			try {
+				CSRUpload::testBlacklist($csr);
+				$this->pass();
+			} catch (Exception  $e) {
+				$this->fail("newly generated CSR should not result in an exception.");
+			}
 		}
 	}
 
 	function testBlacklistCompromisedKey()
 	{
-		$files = array('compromised_1.pem','compromised_2.pem','compromised_3.pem');
-		foreach($files as $f) {
-			$csr = $this->getCSRFromFile($f);
-			try {
-				CSRUpload::testBlacklist($csr);
-				$this->fail("Compromised RSA-key should fail CSRUpload::testBlacklist()");
-			} catch (Exception  $e) {
-				$this->pass();
+		$list = $this->getCompromisedList(2);
+		if ($list) {
+			foreach ($list as $file) {
+				$csr = $this->getCSRFromFile($file);
+				try {
+					CSRUpload::testBlacklist($csr);
+					$this->fail("Compromised RSA-key should fail CSRUpload::testBlacklist() -> $file");
+				} catch (Exception  $e) {
+					$this->pass();
+				}
 			}
+		} else {
+			$this->fail("Missing library of compromised keys, please download and unpack as instructed in " .
+						__FILE__);
 		}
 	}
 
@@ -57,6 +76,33 @@ class TestCSRUpload extends UnitTestCase
 		openssl_csr_export($csr, $csr_out);
 		return $csr_out;
 	}
+
+	private function getCompromisedList($size = 5)
+	{
+		$dir = dirname(__FILE__) . "/files";
+		if (!is_dir($dir)) {
+			echo "$dir not present, cannot retrieve list of compromised keys!";
+			return false;
+		}
+		/* $list = scandir($dir, SCANDIR_SORT_NONE); */ // *much* slower
+		$list = scandir($dir);
+		$lsize = count($list) - 1;
+		$res = array();
+		while($size-- > 0) {
+			do {
+				$f = $list[rand(0, $lsize)];
+			} while (!$this->_isValid($f, 'files'));
+			$res[] = $f;
+		}
+		return $res;
+	}
+
+	private function _isValid($filename, $path)
+	{
+		$file = $path . "/" . $filename;
+		return is_file($file) && is_readable($file);
+	}
+
 	private function getFile($filename)
 	{
 		$path = 'files/'.$filename;
