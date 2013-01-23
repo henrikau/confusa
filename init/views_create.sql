be5e4ce8529525bcbe5733a324c578c297ca87af
Henrik Austad <henrik@austad.us>
2013-01-23 23:29:53 +0100

    drop nren_account_map_view as it is not used
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- init/views_create.sql
+++ init/views_create.sql
@@ -25,19 +25,3 @@ LEFT JOIN
 	nrens n
 ON
 	s.nren_id = n.nren_id;
-
-CREATE OR REPLACE ALGORITHM = TEMPTABLE
-VIEW nren_account_map_view
-	(nren, account_login_name, account_password, account_ivector, ap_name)
-AS SELECT
-	n.name,
-	a.login_name,
-	a.password,
-	a.ivector,
-	a.ap_name
-FROM
-	nrens n
-LEFT JOIN
-	account_map a
-ON
-	n.login_account = a.account_map_id;
