7516a7b039c76bf68128a70a706456c15091514e
Henrik Austad <henrik@austad.us>
2013-05-21 23:46:09 +0200

    tests: use https for tests as well
    Signed-off-by: Henrik Austad <henrik@austad.us>

--- tests/index.php
+++ tests/index.php
@@ -10,8 +10,7 @@ $f = scandir(dirname(__FILE__));
 foreach ($f as $key => $file) {
 	if ((strncmp(substr($file, 0, strlen($start)), $start, strlen($start)) == 0) &&
 		(strncmp(substr($file, -strlen($end)), $end, strlen($end)) == 0)) {
-		/* fixme handle https-connections */
-		$url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $file;
+		$url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . $file;
 		echo "<iframe src=\"$url\" seamless=\"seamless\" width=\"1000\"></iframe><br />\n";
 	}
 }
